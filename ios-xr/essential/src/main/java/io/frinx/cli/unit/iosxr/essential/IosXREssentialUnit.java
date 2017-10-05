6fc82d3fcda1ad10c2b7ce1e469bb85a94ece85e
Jakub Morvay <jmorvay@frinx.io>
2017-10-05 22:23:52 +0200

    Move XR cli parsing to own cli-unit
    Change-Id: Icb7a6ea5ebe9302b07655fafc3875cbba45ce625
    Signed-off-by: Jakub Morvay <jmorvay@frinx.io>

---
+++ ios-xr/essential/src/main/java/io/frinx/cli/unit/iosxr/essential/IosXREssentialUnit.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright © 2017 Frinx and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package io.frinx.cli.unit.iosxr.essential;
+
+import com.google.common.collect.Sets;
+import io.fd.honeycomb.rpc.RpcService;
+import io.fd.honeycomb.translate.impl.read.GenericReader;
+import io.fd.honeycomb.translate.read.registry.ModifiableReaderRegistryBuilder;
+import io.fd.honeycomb.translate.write.registry.ModifiableWriterRegistryBuilder;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.io.Session;
+import io.frinx.cli.io.SessionException;
+import io.frinx.cli.io.SessionInitializationStrategy;
+import io.frinx.cli.registry.spi.TranslateUnit;
+import io.frinx.cli.topology.RemoteDeviceId;
+import io.frinx.cli.unit.iosxr.essential.crud.VersionReader;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ios.essential.rev170520.$YangModuleInfoImpl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.topology.rev170520.CliNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.topology.rev170520.cli.node.credentials.credentials.LoginPassword;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ios.essential.rev170520.Version;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class IosXREssentialUnit implements TranslateUnit {
+    private static final Logger LOG = LoggerFactory.getLogger(IosXREssentialUnit.class);
+
+    @Override
+    public Set<YangModuleInfo> getYangSchemas() {
+        return Sets.newHashSet($YangModuleInfoImpl.getInstance());
+    }
+
+    @Override
+    public SessionInitializationStrategy getInitializer(@Nonnull final RemoteDeviceId id,
+                                                        @Nonnull final CliNode cliNodeConfiguration) {
+        return new IosCliInitializer(cliNodeConfiguration, id);
+    }
+
+    @Override
+    public Set<RpcService<?, ?>> getRpcs(@Nonnull final TranslateUnit.Context context) {
+        return Sets.newHashSet();
+    }
+
+    @Override
+    public void provideHandlers(@Nonnull final ModifiableReaderRegistryBuilder rRegistry,
+                                @Nonnull final ModifiableWriterRegistryBuilder wRegistry,
+                                @Nonnull final TranslateUnit.Context context) {
+        Cli cli = context.getTransport();
+        provideReaders(rRegistry, cli);
+    }
+
+    private void provideReaders(@Nonnull ModifiableReaderRegistryBuilder rRegistry, Cli cli) {
+        // Version
+        rRegistry.add(new GenericReader<>(InstanceIdentifier.create(Version.class), new VersionReader(cli)));
+    }
+
+    @Override
+    public String toString() {
+        return "IOS XR translate unit";
+    }
+
+    /**
+     * Initialize IOS CLI session to be usable by various CRUD and RPC handlers
+     */
+    private static final class IosCliInitializer implements SessionInitializationStrategy {
+        private final CliNode context;
+        private final RemoteDeviceId id;
+
+        IosCliInitializer(CliNode context, RemoteDeviceId id) {
+            this.context = context;
+            this.id = id;
+        }
+
+        @Override
+        public void accept(@Nonnull Session session, @Nonnull String newline) {
+            try {
+                // Enable privileged mode
+                write(session, newline, "enable");
+                // Fill in password
+                checkArgument(context.getCredentials() instanceof LoginPassword,
+                        "{}: Unable to handle credentials type of: %s", id, context.getCredentials());
+                String password = ((LoginPassword) context.getCredentials()).getPassword();
+                write(session, newline, password);
+                // Set terminal length to 0 to prevent "--More--" situation
+                write(session, newline, "terminal length 0");
+                // Read the output
+                session.readUntilTimeout(1);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            } catch (SessionException | ExecutionException | TimeoutException e) {
+                LOG.warn("{}: Unable to initialize device", id, e);
+                throw new IllegalStateException(id + ": Unable to initialize device", e);
+            }
+        }
+    }
+}
+
--- ios/essential/src/test/java/io/frinx/cli/unit/ios/essential/crud/VersionReaderTest.java
+++ ios/essential/src/test/java/io/frinx/cli/unit/ios/essential/crud/VersionReaderTest.java
@@ -115,93 +115,6 @@ public class VersionReaderTest {
                     "Configuration register is 0x0\n" +
                     "\n";
 
-    private static final String SH_VERSION_XR = "Tue Oct  3 14:02:23.311 UTC\n" +
-                    "\n" +
-                    "Cisco IOS XR Software, Version 6.1.2[Default]\n" +
-                    "Copyright (c) 2016 by Cisco Systems, Inc.\n" +
-                    "\n" +
-                    "ROM: GRUB, Version 1.99(0), DEV RELEASE\n" +
-                    "\n" +
-                    "PE3 uptime is 5 days, 8 hours, 22 minutes\n" +
-                    "System image file is \"bootflash:disk0/xrvr-os-mbi-6.1.2/mbixrvr-rp.vm\"\n" +
-                    "\n" +
-                    "cisco IOS XRv Series (Pentium Celeron Stepping 3) processor with 3145215K bytes of memory.\n" +
-                    "Pentium Celeron Stepping 3 processor at 2503MHz, Revision 2.174\n" +
-                    "IOS XRv Chassis\n" +
-                    "\n" +
-                    "1 Management Ethernet\n" +
-                    "6 GigabitEthernet\n" +
-                    "97070k bytes of non-volatile configuration memory.\n" +
-                    "866M bytes of hard disk.\n" +
-                    "2321392k bytes of disk0: (Sector size 512 bytes).\n" +
-                    "\n" +
-                    "Configuration register on node 0/0/CPU0 is 0x2102\n" +
-                    "Boot device on node 0/0/CPU0 is disk0:\n" +
-                    "Package active on node 0/0/CPU0:\n" +
-                    "iosxr-infra, V 6.1.2[Default], Cisco Systems, at disk0:iosxr-infra-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "iosxr-fwding, V 6.1.2[Default], Cisco Systems, at disk0:iosxr-fwding-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "iosxr-routing, V 6.1.2[Default], Cisco Systems, at disk0:iosxr-routing-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "iosxr-ce, V 6.1.2[Default], Cisco Systems, at disk0:iosxr-ce-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "xrvr-os-mbi, V 6.1.2[Default], Cisco Systems, at disk0:xrvr-os-mbi-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:53 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "xrvr-base, V 6.1.2[Default], Cisco Systems, at disk0:xrvr-base-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "xrvr-fwding, V 6.1.2[Default], Cisco Systems, at disk0:xrvr-fwding-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "xrvr-mgbl-x, V 6.1.2[Default], Cisco Systems, at disk0:xrvr-mgbl-x-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:10 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "iosxr-mpls, V 6.1.2[Default], Cisco Systems, at disk0:iosxr-mpls-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "iosxr-mgbl, V 6.1.2[Default], Cisco Systems, at disk0:iosxr-mgbl-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "iosxr-mcast, V 6.1.2[Default], Cisco Systems, at disk0:iosxr-mcast-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "xrvr-mcast-supp, V 6.1.2[Default], Cisco Systems, at disk0:xrvr-mcast-supp-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:02 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "iosxr-bng, V 6.1.2[Default], Cisco Systems, at disk0:iosxr-bng-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:04 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "xrvr-bng-supp, V 6.1.2[Default], Cisco Systems, at disk0:xrvr-bng-supp-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:04 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "iosxr-security, V 6.1.2[Default], Cisco Systems, at disk0:iosxr-security-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:31:55 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie\n" +
-                    "\n" +
-                    "xrvr-fullk9-x, V 6.1.2[Default], Cisco Systems, at disk0:xrvr-fullk9-x-6.1.2\n" +
-                    "    Built on Fri Nov 11 11:32:56 UTC 2016\n" +
-                    "    By iox-lnx-009 in /auto/srcarchive11/production/6.1.2/xrvr/workspace for pie";
-
     @Test
     public void testParseXEShowVersion() {
         VersionBuilder expected = new VersionBuilder();
@@ -230,20 +143,5 @@ public class VersionReaderTest {
         VersionBuilder actual = new VersionBuilder();
         VersionReader.parseShowVersion(SH_VERSION, actual);
         assertEquals(expected.build(), actual.build());
-    }
-
-    @Test
-    public void testParseXRShowVersion() {
-        VersionBuilder expectedVersion = new VersionBuilder();
-        expectedVersion.setConfReg("0x2102")
-                .setOsFamily("Cisco IOS XR")
-                .setPlatform("cisco IOS XRv Series (Pentium Celeron Stepping 3) processor")
-                .setOsVersion("6.1.2[Default]")
-                .setSysMemory("3145215K")
-                .setSysImage("bootflash:disk0/xrvr-os-mbi-6.1.2/mbixrvr-rp.vm");
-
-        VersionBuilder actualVersion = new VersionBuilder();
-        VersionReader.parseShowVersion(SH_VERSION_XR, actualVersion);
-        assertEquals(expectedVersion.build(), actualVersion.build());
     }
 }
