fe2a0fac111ea27a10258bfae9575ef6293161a5
Maros Marsalek <mmarsalek@frinx.io>
2017-10-11 14:17:47 +0200

    Remove invalid check from RibReader
    Change-Id: I15ffc5cf4ef55d912e7365bf0ab713aa80bb12ca
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- ios/bgp/src/main/java/io/frinx/cli/ios/bgp/handler/RibReader.java
+++ ios/bgp/src/main/java/io/frinx/cli/ios/bgp/handler/RibReader.java
@@ -8,15 +8,16 @@
 
 package io.frinx.cli.ios.bgp.handler;
 
+import com.google.common.annotations.VisibleForTesting;
+import io.fd.honeycomb.translate.read.ReadContext;
+import io.fd.honeycomb.translate.read.ReadFailedException;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.unit.utils.CliReader;
+import io.frinx.cli.unit.utils.ParsingUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
-
-import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev170202.BgpOriginAttrType;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev170202.IPV4UNICAST;
-import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.network.instance.protocols.Protocol;
-import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.network.instance.protocols.ProtocolKey;
-import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.rib.bgp.rev161017.BgpLocRibCommonKeys.Origin;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.rib.bgp.rev161017.bgp.rib.top.BgpRib;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.rib.bgp.rev161017.bgp.rib.top.BgpRibBuilder;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.rib.bgp.rev161017.bgp.rib.top.bgp.rib.AfiSafisBuilder;
@@ -33,17 +34,9 @@ import org.opendaylight.yangtools.concepts.Builder;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
-import com.google.common.annotations.VisibleForTesting;
-
-import io.fd.honeycomb.translate.read.ReadContext;
-import io.fd.honeycomb.translate.read.ReadFailedException;
-import io.frinx.cli.io.Cli;
-import io.frinx.cli.unit.utils.CliReader;
-import io.frinx.cli.unit.utils.ParsingUtils;
-
 public class RibReader implements CliReader<BgpRib, BgpRibBuilder> {
 
-    private static final String SH_VERSION = "sh ip bgp";
+    private static final String SH_IP_BGP = "sh ip bgp";
     private static final Pattern ROUTE_LINE = Pattern.compile("(?<statusCodes>[sdh\\*>irSmbfxac\\s]+) (?<prefix>[\\S]+) (?<nextHop>.+) (?:.*) (?<origin>[ie\\?])");
 
     private Cli cli;
@@ -65,12 +58,7 @@ public class RibReader implements CliReader<BgpRib, BgpRibBuilder> {
     @Override
     public void readCurrentAttributes(InstanceIdentifier<BgpRib> id, BgpRibBuilder builder, ReadContext ctx)
     throws ReadFailedException {
-        ProtocolKey protocolKey = id.firstKeyOf(Protocol.class);
-        if (!protocolKey.getIdentifier().equals(BgpProtocolReader.TYPE)) {
-            return;
-        }
-
-        parseRib(blockingRead(SH_VERSION, cli, id), builder);
+        parseRib(blockingRead(SH_IP_BGP, cli, id), builder);
     }
 
     @VisibleForTesting
