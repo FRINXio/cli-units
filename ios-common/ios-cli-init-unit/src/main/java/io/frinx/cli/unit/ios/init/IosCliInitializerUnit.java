1a2ed77c7943b23df07201e0e262c91945f75e35
Jakub Morvay <jmorvay@frinx.io>
2017-11-06 15:51:48 +0100

    Make IosCliInitializer more robust
    IosCliInitializer tries to enter user EXEC privileged mode by issuing
    'enable' command and immediately enters enable password, no matter if the
    password is actually requested. If the password is not requested, this can
    trigger DNS resolution and causes delays.
    
    Enter password, only if the password is requested. Also improve logs and
    handling of unsuccessful cli initialization attempts.
    
    Change-Id: I7e442fbad3d136156fed03a20ee82073230bd95c
    Signed-off-by: Jakub Morvay <jmorvay@frinx.io>

--- ios-common/ios-cli-init-unit/src/main/java/io/frinx/cli/unit/ios/init/IosCliInitializerUnit.java
+++ ios-common/ios-cli-init-unit/src/main/java/io/frinx/cli/unit/ios/init/IosCliInitializerUnit.java
@@ -17,10 +17,12 @@ import io.fd.honeycomb.translate.write.registry.ModifiableWriterRegistryBuilder;
 import io.frinx.cli.io.Session;
 import io.frinx.cli.io.SessionException;
 import io.frinx.cli.io.SessionInitializationStrategy;
+import io.frinx.cli.io.impl.cli.PromptResolutionStrategy;
 import io.frinx.cli.registry.api.TranslationUnitCollector;
 import io.frinx.cli.registry.spi.TranslateUnit;
 import io.frinx.cli.topology.RemoteDeviceId;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.Device;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.DeviceIdBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.topology.rev170520.CliNode;
@@ -122,6 +124,13 @@ public class IosCliInitializerUnit  implements TranslateUnit {
      * Initialize IOS CLI session to be usable by various CRUD and RPC handlers
      */
     private static final class IosCliInitializer implements SessionInitializationStrategy {
+        private static final String PASSWORD_PROMPT = "Password:";
+        private static final String PRIVILEGED_PROMPT_SUFFIX = "#";
+        private static final String ENABLE_COMMAND = "enable";
+        private static final String SET_TERMINAL_LENGTH_COMMAND = "terminal length 0";
+        private static final int WRITE_TIMEOUT_SECONDS = 10;
+        private static final int READ_TIMEOUT_SECONDS = 1;
+
         private final CliNode context;
         private final RemoteDeviceId id;
 
@@ -133,23 +142,76 @@ public class IosCliInitializerUnit  implements TranslateUnit {
         @Override
         public void accept(@Nonnull Session session, @Nonnull String newline) {
             try {
-                // Enable privileged mode
-                write(session, newline, "enable");
-                // Fill in password
-                checkArgument(context.getCredentials() instanceof LoginPassword,
-                        "{}: Unable to handle credentials type of: %s", id, context.getCredentials());
-                String password = ((LoginPassword) context.getCredentials()).getPassword();
-                write(session, newline, password);
+
                 // Set terminal length to 0 to prevent "--More--" situation
-                write(session, newline, "terminal length 0");
-                // Read the output
-                session.readUntilTimeout(1);
+                LOG.debug("{}: Setting terminal length to 0 to prevent \"--More--\" situation", id);
+                write(session, newline, SET_TERMINAL_LENGTH_COMMAND);
+                session.readUntilTimeout(READ_TIMEOUT_SECONDS);
+
+                // If already in privileged mode, don't do anything else
+                if (PromptResolutionStrategy.ENTER_AND_READ.resolvePrompt(session, newline).trim()
+                        .endsWith(PRIVILEGED_PROMPT_SUFFIX)) {
+                    LOG.info("{}: IOS cli session already initialized", id);
+                    return;
+                }
+
+                // Enable privileged mode
+                tryToEnterPrivilegedMode(session, newline);
+
+                // Check if we are actually in privileged mode
+                String prompt = PromptResolutionStrategy.ENTER_AND_READ.resolvePrompt(session, newline).trim();
+
+                if (!prompt.endsWith(PRIVILEGED_PROMPT_SUFFIX)) {
+                    LOG.warn("{}: IOS cli session initialization failed to enter privileged mode. Current prompt {}",
+                            id, prompt);
+                } else {
+                    LOG.info("{}: IOS cli session initialized successfully", id);
+                }
+
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 throw new RuntimeException(e);
             } catch (SessionException | ExecutionException | TimeoutException e) {
                 LOG.warn("{}: Unable to initialize device", id, e);
                 throw new IllegalStateException(id + ": Unable to initialize device", e);
+            }
+        }
+
+        private void tryToEnterPrivilegedMode(@Nonnull Session session, @Nonnull String newline)
+                throws InterruptedException, ExecutionException, TimeoutException {
+
+            write(session, newline, ENABLE_COMMAND);
+            String enableCommandOutput = session.readUntilTimeout(READ_TIMEOUT_SECONDS).trim();
+
+            // password is requested
+            // TODO When reading from session, we can see all previously
+            // unread output, that is also previous commands, prompt, etc.
+            // That's why we have to use String#endWith method to check
+            // if command's output matches something.
+            // Can we hide this in session's API?
+            if (enableCommandOutput.endsWith(PASSWORD_PROMPT)) {
+                checkArgument(context.getCredentials() instanceof LoginPassword,
+                        "%s: Unable to handle credentials type of: %s", id, context.getCredentials());
+                String password = ((LoginPassword) context.getCredentials()).getPassword();
+                write(session, newline, password);
+
+                String output = session.readUntilTimeout(READ_TIMEOUT_SECONDS).trim();
+                if (output.endsWith(PASSWORD_PROMPT)) {
+                    LOG.warn("{}: Specified enable password is not correct", id);
+
+                    // We have entered incorrect password and we can be asked for
+                    // correct one multiple times. Just skip those requests.
+                    while (output.endsWith(PASSWORD_PROMPT)) {
+                        session.write(newline).toCompletableFuture().get(WRITE_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+                        output = session.readUntilTimeout(READ_TIMEOUT_SECONDS).trim();
+                    }
+
+                } else {
+                    LOG.debug("{}: Entering enable password resulted in output: {}", id, output);
+                }
+            } else {
+                LOG.debug("{}: enable command did not resulted in password prompt, enable command output: {}",
+                        id, enableCommandOutput);
             }
         }
     }
