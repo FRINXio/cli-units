bcdf58d27d95a42bf024f5645bc3b008874b5182
Michal Cmarada <mcmarada@frinx.io>
2017-11-28 16:19:04 +0100

    Fix staticRoute
    this is fix for https://frinxhelpdesk.atlassian.net/browse/MU-90
    
    depends on: https://gerrit.frinx.io/#/c/1674/
    
    Change-Id: I7be725fe98f16a50511b5ca8144219a42cafe9a7
    Signed-off-by: Michal Cmarada <mcmarada@frinx.io>

--- ios/local-routing/src/test/java/io/frinx/cli/ios/local/routing/handlers/StaticReaderTest.java
+++ ios/local-routing/src/test/java/io/frinx/cli/ios/local/routing/handlers/StaticReaderTest.java
@@ -9,6 +9,7 @@
 package io.frinx.cli.ios.local.routing.handlers;
 
 import static io.frinx.cli.ios.local.routing.handlers.StaticReader.parseStaticPrefixes;
+import static io.frinx.openconfig.network.instance.NetworInstance.DEFAULT_NETWORK_NAME;
 import static org.junit.Assert.assertEquals;
 
 import com.google.common.collect.Lists;
@@ -20,51 +21,42 @@ import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.types.inet.rev17040
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.types.inet.rev170403.Ipv4Prefix;
 
 public class StaticReaderTest {
-    private static String STATIC_OUTPUT = "Codes: M - Manual static, A - AAA download, N - IP NAT, D - DHCP,\n" +
-            "       G - GPRS, V - Crypto VPN, C - CASA, P - Channel interface processor,\n" +
-            "       B - BootP, S - Service selection gateway\n" +
-            "       DN - Default Network, T - Tracking object\n" +
-            "       L - TL1, E - OER, I - iEdge\n" +
-            "       D1 - Dot1x Vlan Network, K - MWAM Route\n" +
-            "       PP - PPP default route, MR - MRIPv6, SS - SSLVPN\n" +
-            "       H - IPe Host, ID - IPe Domain Broadcast\n" +
-            "       U - User GPRS, TE - MPLS Traffic-eng, LI - LIIN\n" +
-            "       IR - ICMP Redirect\n" +
-            "Codes in []: A - active, N - non-active, B - BFD-tracked, D - Not Tracked, P - permanent\n" +
-            "\n" +
-            "Static local RIB for default \n" +
-            "\n" +
-            "M  10.10.10.0/24 [1/0] via 10.20.1.2 [N]\n" +
-            "M  12.12.12.0/24 [1/0] via 172.16.18.1 [N]\n" +
-            "M                [1/0] via 99.99.99.0 [N]\n" +
-            "M                [1/0] via 99.99.99.1 [N]\n" +
-            "M                [1/0] via 14.14.14.2 [A]\n" +
-            "M                [100/0] via 192.168.1.2 [N]\n" +
-            "M                [100/0] via 192.168.1.1 [N]\n" +
-            "M  12.12.13.0/24 [1/0] via 192.168.1.1 [A]\n" +
-            "M  13.13.13.0/24 [1/0] via Loopback99 [A]\n" +
-            "M                [100/0] via 192.168.1.1 [N]\n" +
-            "M  14.14.14.0/24 [100/0] via 192.168.1.1 [A]\n" +
-            "M                [100/0] via GigabitEthernet1 [A]\n" +
-            "M  15.15.15.0/24 [100/0] via 192.168.1.1 [A]\n" +
-            "M  16.16.16.0/24 [50/0] via 12.12.12.1 [A]\n" +
-            "M                [50/0] via 12.12.12.2 [A]\n" +
-            "M  143.6.7.0/24 [2/0] via GigabitEthernet1 24.5.6.5 [A]\n" +
-            "M               [3/0] via Null0 [N]\n" +
-            "M               [123/0] via 8.8.8.8 [N]\n" +
-            "M  192.0.2.0/24 [1/0] via 192.0.2.10 [N]\n" +
-            "M  192.168.2.0/24 [1/0] via 192.168.7.65 [N]\n" +
-            "M  192.168.5.0/24 [1/0] via 192.168.7.65 [N]";
+
+    private static final String VRF_NAME = "TMP";
+    private static String STATIC_OUTPUT =
+            "ip route 10.255.1.0 255.255.255.0 Null0\n" +
+            "ip route 10.255.1.0 255.255.255.0 192.168.1.5\n" +
+            "ip route 10.255.2.0 255.255.255.0 192.168.1.5\n" +
+            "ip route 12.255.1.0 255.255.255.240 192.168.1.24\n" +
+            "ip route 192.0.2.0 255.255.255.0 15.3.0.1\n" +
+            "ip route 192.0.2.0 255.255.255.0 15.3.0.3\n" +
+            "ip route 192.0.2.0 255.255.255.0 6.3.0.3\n" +
+            "ip route 192.0.2.0 255.255.255.0 192.0.2.12\n" +
+            "ip route 192.0.2.0 255.255.255.0 192.0.2.10\n" +
+            "ip route 192.0.2.0 255.255.255.0 192.0.2.10\n" +
+            "ip route vrf TMP 192.0.3.0 255.255.255.0 10.10.10.10\n" +
+            "ip route vrf TMP 192.0.2.0 255.255.255.0 10.10.10.20\n" +
+            "ip route vrf AAA 192.0.4.0 255.255.255.0 10.10.10.30\n";
 
     private static final List<StaticKey> EXPECTED_IDS =
-            Lists.newArrayList("10.10.10.0/24", "12.12.12.0/24","12.12.13.0/24", "13.13.13.0/24",
-                    "14.14.14.0/24", "15.15.15.0/24", "16.16.16.0/24", "143.6.7.0/24", "192.0.2.0/24", "192.168.2.0/24",
-                    "192.168.5.0/24")
+            Lists.newArrayList("10.255.1.0/24", "10.255.2.0/24", "12.255.1.0/28", "192.0.2.0/24")
                     .stream()
                     .map(value -> new StaticKey(new IpPrefix(new Ipv4Prefix(value))))
                     .collect(Collectors.toList());
+
+    private static final List<StaticKey> EXPECTED_IDS_VRF =
+        Lists.newArrayList("192.0.3.0/24", "192.0.2.0/24")
+            .stream()
+            .map(value -> new StaticKey(new IpPrefix(new Ipv4Prefix(value))))
+            .collect(Collectors.toList());
+
     @Test
     public void testParseStaticPrefixes() {
-        assertEquals(EXPECTED_IDS, parseStaticPrefixes(STATIC_OUTPUT));
+        assertEquals(EXPECTED_IDS, parseStaticPrefixes(STATIC_OUTPUT, DEFAULT_NETWORK_NAME));
+    }
+
+    @Test
+    public void testParseStaticPrefixesVrf() {
+        assertEquals(EXPECTED_IDS_VRF, parseStaticPrefixes(STATIC_OUTPUT, VRF_NAME));
     }
 }
