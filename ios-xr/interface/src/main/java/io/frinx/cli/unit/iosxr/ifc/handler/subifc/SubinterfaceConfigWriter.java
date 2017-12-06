90b0b4d3dc3309b0feac5c18c70028874822f110
Jakub Morvay <jmorvay@frinx.io>
2017-12-06 18:40:45 +0100

    Improve .0 subifc handling in ios-xr-interface-unit
    .0 subinterface is special subinterface that holds IP configuration of
    its parent interface. Subinterface is auto-generated for all interfaces
    and only IP configuration is allowed on this special subinterface.
    
    Change-Id: Ia1edaa6c62313e60b27ca9e80a987c29942f5986
    Signed-off-by: Jakub Morvay <jmorvay@frinx.io>

--- ios-xr/interface/src/main/java/io/frinx/cli/unit/iosxr/ifc/handler/subifc/SubinterfaceConfigWriter.java
+++ ios-xr/interface/src/main/java/io/frinx/cli/unit/iosxr/ifc/handler/subifc/SubinterfaceConfigWriter.java
@@ -8,8 +8,10 @@
 
 package io.frinx.cli.unit.iosxr.ifc.handler.subifc;
 
+import static io.frinx.cli.unit.iosxr.ifc.handler.subifc.SubinterfaceReader.ZERO_SUBINTERFACE_ID;
 import static io.frinx.cli.unit.iosxr.ifc.handler.subifc.SubinterfaceReader.getSubinterfaceName;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Sets;
 import io.fd.honeycomb.translate.util.RWUtils;
 import io.fd.honeycomb.translate.write.WriteContext;
@@ -19,6 +21,7 @@ import io.frinx.cli.unit.utils.CliWriter;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.interfaces.Interface;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.subinterfaces.top.subinterfaces.Subinterface;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.subinterfaces.top.subinterfaces.subinterface.Config;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana._if.type.rev140508.EthernetCsmacd;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana._if.type.rev140508.Ieee8023adLag;
@@ -44,6 +47,16 @@ public class SubinterfaceConfigWriter implements CliWriter<Config> {
                                        @Nonnull Config data,
                                        @Nonnull WriteContext writeContext) throws WriteFailedException {
         InstanceIdentifier<Interface> parentIfcId = RWUtils.cutId(id, Interface.class);
+
+        // If we are creating new interface, allow empty config for .0 subifc
+        if (!writeContext.readBefore(parentIfcId).isPresent()
+                && id.firstKeyOf(Subinterface.class).getIndex() == ZERO_SUBINTERFACE_ID) {
+            Preconditions.checkArgument(data.getDescription() == null,
+                    "Cannot change description for .0 subinterface");
+            return;
+        }
+
+        checkNotZeroSubinterface(id);
         Class<? extends InterfaceType> parentIfcType = writeContext.readAfter(parentIfcId).get().getConfig().getType();
 
         if(isSupportedType(parentIfcType)) {
@@ -60,6 +73,17 @@ public class SubinterfaceConfigWriter implements CliWriter<Config> {
         }
     }
 
+    static void checkNotZeroSubinterface(@Nonnull InstanceIdentifier<?> id) throws WriteFailedException {
+        Long subifcIndex = id.firstKeyOf(Subinterface.class).getIndex();
+
+        // TODO Should we parametrize error message with more info?
+        // Like what do we want to change etc.
+        // TODO At least debug log this?
+        Preconditions.checkArgument(subifcIndex != ZERO_SUBINTERFACE_ID,
+                "Only IP configuration is allowed for .%s subinterface", subifcIndex);
+
+    }
+
     @Override
     public void updateCurrentAttributes(@Nonnull InstanceIdentifier<Config> id,
                                         @Nonnull Config dataBefore,
@@ -73,7 +97,16 @@ public class SubinterfaceConfigWriter implements CliWriter<Config> {
     public void deleteCurrentAttributes(@Nonnull InstanceIdentifier<Config> id,
                                         @Nonnull Config data,
                                         @Nonnull WriteContext writeContext) throws WriteFailedException {
+
         InstanceIdentifier<Interface> parentIfcId = RWUtils.cutId(id, Interface.class);
+
+        // if deleting parent interface allow deleting also .0 subifc
+        if (!writeContext.readAfter(parentIfcId).isPresent()
+                && id.firstKeyOf(Subinterface.class).getIndex() == ZERO_SUBINTERFACE_ID) {
+            return;
+        }
+
+        checkNotZeroSubinterface(id);
         Class<? extends InterfaceType> parentIfcType = writeContext.readBefore(parentIfcId).get().getConfig().getType();
 
         if(isSupportedType(parentIfcType)) {
