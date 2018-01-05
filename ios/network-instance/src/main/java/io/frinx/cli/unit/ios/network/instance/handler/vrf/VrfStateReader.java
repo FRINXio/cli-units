920f1d6136736cec3e72d1471d262677a0c3b197
Maros Marsalek <mmarsalek@frinx.io>
2018-01-05 16:21:05 +0100

    State vs Config reader re-usability pattern
    Change-Id: I38a76819af4c1e63a6cfc467476dc4172e9244a5
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- ios/network-instance/src/main/java/io/frinx/cli/unit/ios/network/instance/handler/vrf/VrfStateReader.java
+++ ios/network-instance/src/main/java/io/frinx/cli/unit/ios/network/instance/handler/vrf/VrfStateReader.java
@@ -10,14 +10,15 @@ package io.frinx.cli.unit.ios.network.instance.handler.vrf;
 
 import io.fd.honeycomb.translate.read.ReadContext;
 import io.fd.honeycomb.translate.read.ReadFailedException;
+import io.fd.honeycomb.translate.util.RWUtils;
 import io.frinx.cli.io.Cli;
 import io.frinx.cli.registry.common.CompositeReader;
 import io.frinx.cli.unit.utils.CliOperReader;
+import io.frinx.openconfig.openconfig.network.instance.IIDs;
 import javax.annotation.Nonnull;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.NetworkInstance;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.network.instance.Config;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.network.instance.State;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.network.instance.StateBuilder;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.network.instance.types.rev170228.L3VRF;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 public class VrfStateReader implements CliOperReader<State, StateBuilder>,
@@ -33,15 +34,9 @@ public class VrfStateReader implements CliOperReader<State, StateBuilder>,
     public void readCurrentAttributes(@Nonnull InstanceIdentifier<State> instanceIdentifier,
                                       @Nonnull StateBuilder stateBuilder,
                                       @Nonnull ReadContext readContext) throws ReadFailedException {
-        if (isVrf(instanceIdentifier, readContext)) {
-            stateBuilder.setName(instanceIdentifier.firstKeyOf(NetworkInstance.class).getName());
-            stateBuilder.setType(L3VRF.class);
-
-            // TODO set other attributes i.e. description
-        }
+        // Set config attributes
+        stateBuilder.fieldsFrom(readContext.read(RWUtils.cutId(instanceIdentifier, IIDs.NE_NETWORKINSTANCE).child(Config.class)).get());
+        // TODO set state attributes
     }
 
-    private boolean isVrf(InstanceIdentifier<State> id, ReadContext readContext) throws ReadFailedException {
-        return VrfReader.getAllIds(id, readContext, cli, this).contains(id.firstKeyOf(NetworkInstance.class));
-    }
 }
