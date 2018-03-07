12ec54c2c4e9a7e3b4b11ab0ebfc3fd5aa9b93a7
Dana Kutenicsova <dkutenicsova@frinx.io>
2018-03-07 10:46:18 +0100

    MCCAS-27 : QoS unit structure
    Change-Id: If4af48e6fa1263d482f52d49b7e9926b3fcbfe44
    Signed-off-by: Dana Kutenicsova <dkutenicsova@frinx.io>

---
+++ ios-xr/qos/src/main/java/io/frinx/cli/iosxr/qos/XRQoSUnit.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright © 2018 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.iosxr.qos;
+
+import com.google.common.collect.Sets;
+import io.fd.honeycomb.rpc.RpcService;
+import io.fd.honeycomb.translate.read.registry.ModifiableReaderRegistryBuilder;
+import io.fd.honeycomb.translate.write.registry.ModifiableWriterRegistryBuilder;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.registry.api.TranslationUnitCollector;
+import io.frinx.cli.registry.spi.TranslateUnit;
+import java.util.Collections;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.qos.rev161216.$YangModuleInfoImpl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.Device;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.DeviceIdBuilder;
+import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
+
+public class XRQoSUnit implements TranslateUnit {
+
+    private static final Device IOS_ALL = new DeviceIdBuilder()
+            .setDeviceType("ios xr")
+            .setDeviceVersion("*")
+            .build();
+
+    private final TranslationUnitCollector registry;
+    private TranslationUnitCollector.Registration reg;
+
+    public XRQoSUnit(@Nonnull final TranslationUnitCollector registry) {
+        this.registry = registry;
+    }
+
+    public void init() {
+        reg = registry.registerTranslateUnit(IOS_ALL, this);
+    }
+
+    public void close() {
+        if (reg != null) {
+            reg.close();
+        }
+    }
+
+    @Override
+    public Set<RpcService<?, ?>> getRpcs(@Nonnull Context context) {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public void provideHandlers(@Nonnull ModifiableReaderRegistryBuilder rRegistry,
+                                @Nonnull ModifiableWriterRegistryBuilder wRegistry,
+                                @Nonnull Context context) {
+        Cli cli = context.getTransport();
+        provideReaders(rRegistry, cli);
+        provideWriters(wRegistry, cli);
+    }
+
+    private void provideWriters(ModifiableWriterRegistryBuilder wRegistry, Cli cli) {
+
+    }
+
+    private void provideReaders(@Nonnull ModifiableReaderRegistryBuilder rRegistry, Cli cli) {
+
+    }
+
+    @Override
+    public Set<YangModuleInfo> getYangSchemas() {
+        return Sets.newHashSet($YangModuleInfoImpl.getInstance());
+    }
+
+    @Override
+    public String toString() {
+        return "IOS XR QoS unit";
+    }
+}
