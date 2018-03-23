ee6542a1cbb6a982052774206fce8669e554f0fb
thodor <thodor@frinx.io>
2018-03-23 17:39:58 +0100

    FI-117 : Added new module in IOS-XR for configmetadata
    - Created reader for Last fingerprint for IOS-XR devices
    - Added to module to features
    
    Change-Id: I423732db0a70474ab7e35b4bacc19f4eefbe1785
    Signed-off-by: thodor <thodor@frinx.io>

---
+++ ios-xr/configuration-metadata/src/main/java/io/frinx/cli/iosxr/conf/handler/ConfigMetadataReader.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright © 2018 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.iosxr.conf.handler;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.fd.honeycomb.translate.read.ReadContext;
+import io.fd.honeycomb.translate.read.ReadFailedException;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.unit.utils.CliOperReader;
+import io.frinx.cli.unit.utils.ParsingUtils;
+import java.text.ParseException;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.configuration.metadata.rev700101.metadata.ConfigurationMetadata;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.configuration.metadata.rev700101.metadata.ConfigurationMetadataBuilder;
+import org.opendaylight.yangtools.concepts.Builder;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class ConfigMetadataReader implements CliOperReader<ConfigurationMetadata, ConfigurationMetadataBuilder> {
+
+    private static final String SHOW_LAST_COMMIT_TIME = "show configuration commit list | utility egrep \"^1 \"";
+    private static final String DATE_REGEX = "\\s*.+\\s{2,}.+\\s{2,}.+\\s{2,}.+\\s{2,}.+\\s{3,}(?<time>.+)\\s*";
+    private static final Pattern pattern = Pattern.compile(DATE_REGEX);
+    private final Cli cli;
+
+    public ConfigMetadataReader(final Cli cli) {
+        this.cli = cli;
+    }
+
+    @VisibleForTesting
+    static Optional<String> getLastConfigurationFingerprint(String TimeFormat) throws ParseException {
+
+        return ParsingUtils.parseField(TimeFormat, 0, pattern::matcher, m -> m.group("time"));
+    }
+
+    @Override
+    public void readCurrentAttributes(@Nonnull InstanceIdentifier<ConfigurationMetadata> instanceIdentifier, @Nonnull ConfigurationMetadataBuilder configurationMetadataBuilder, @Nonnull ReadContext readContext) throws ReadFailedException {
+        String output = blockingRead(SHOW_LAST_COMMIT_TIME, cli, instanceIdentifier, readContext);
+
+        try {
+            Optional<String> data = getLastConfigurationFingerprint(output);
+            data.ifPresent(configurationMetadataBuilder::setLastConfigurationFingerprint);
+        } catch (ParseException e) {
+            throw new ReadFailedException(instanceIdentifier, e);
+        }
+    }
+
+    @Override
+    public void merge(@Nonnull Builder<? extends DataObject> builder, @Nonnull ConfigurationMetadata configurationMetadata) {
+        // NOOP, root reader
+    }
+}
+
