80c1029a881e21ac8cc23fb8b97001602aa8d7dc
thodor <thodor@frinx.io>
2018-04-24 16:33:39 +0200

    FI-117 Added new module in IOS for configmetadata
    - Created reader for Last fingerprint for IOS-XE devices
    - Added to module to features
    
    Change-Id: Iac8dfe541449d7b90de20340d934088fb1f462bf
    Signed-off-by: thodor <thodor@frinx.io>

---
+++ ios/configuration-metadata/src/main/java/io/frinx/cli/ios/conf/ConfigurationUnit.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright © 2018 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.ios.conf;
+
+
+import com.google.common.collect.Sets;
+import io.fd.honeycomb.rpc.RpcService;
+import io.fd.honeycomb.translate.impl.read.GenericOperReader;
+import io.fd.honeycomb.translate.read.registry.ModifiableReaderRegistryBuilder;
+import io.fd.honeycomb.translate.write.registry.ModifiableWriterRegistryBuilder;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.ios.conf.handler.ConfigMetadataReader;
+import io.frinx.cli.registry.api.TranslationUnitCollector;
+import io.frinx.cli.registry.spi.TranslateUnit;
+import io.frinx.openconfig.openconfig.configuration.metadata.IIDs;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.configuration.metadata.rev700101.$YangModuleInfoImpl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.Device;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.DeviceIdBuilder;
+import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
+
+public class ConfigurationUnit implements TranslateUnit {
+
+    private static final Device IOS_ALL = new DeviceIdBuilder()
+            .setDeviceType("ios")
+            .setDeviceVersion("*")
+            .build();
+
+    private final TranslationUnitCollector registry;
+    private TranslationUnitCollector.Registration reg;
+
+    public ConfigurationUnit(@Nonnull final TranslationUnitCollector registry) {
+        this.registry = registry;
+    }
+
+    public void init() {
+        reg = registry.registerTranslateUnit(IOS_ALL, this);
+    }
+
+    public void close() {
+        if (reg != null) {
+            reg.close();
+        }
+    }
+
+    @Override
+    public Set<YangModuleInfo> getYangSchemas() {
+        return Sets.newHashSet($YangModuleInfoImpl.getInstance());
+    }
+
+    @Override
+    public Set<RpcService<?, ?>> getRpcs(@Nonnull final TranslateUnit.Context context) {
+        return Sets.newHashSet();
+    }
+
+    @Override
+    public void provideHandlers(@Nonnull ModifiableReaderRegistryBuilder rRegistry,
+                                @Nonnull ModifiableWriterRegistryBuilder wRegistry,
+                                @Nonnull Context context) {
+        Cli cli = context.getTransport();
+        provideReaders(rRegistry, cli);
+    }
+
+    private void provideReaders(@Nonnull ModifiableReaderRegistryBuilder rRegistry, Cli cli) {
+        rRegistry.add(new GenericOperReader<>(IIDs.CONFIGURATIONMETADATA, new ConfigMetadataReader(cli)));
+    }
+
+    @Override
+    public String toString() {
+        return "IOS Configuration Metadata unit";
+    }
+}
---
+++ ios/configuration-metadata/src/test/java/io/frinx/cli/ios/conf/handler/ConfigMetadataReaderTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2018 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.ios.conf.handler;
+
+import static org.junit.Assert.assertFalse;
+
+import java.util.Optional;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConfigMetadataReaderTest {
+
+    private static final String OUTPUT = "**Apr 23 11:20:43.936: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:20:44.089: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:20:44.993: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:20:45.457: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:20:45.903: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:23:34.958: %SYS-5-CONFIG_I: Configured from console by cisco on vty1 (192.168.1.41)\n" +
+            "*Apr 23 11:24:16.670: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:24:16.829: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:24:17.717: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:24:18.169: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:24:18.617: %SYS-5-CONFIG_I: Configured from console by cisco on vty0 (192.168.1.41)\n" +
+            "*Apr 23 11:33:52.247: %SYS-5-CONFIG_I: Configured from console by cisco on vty1 (192.168.1.41)\n" +
+            "*Apr 23 14:00:01.896: %SYS-5-CONFIG_I: Configured from console by cisco on vty1 (192.168.1.43)\n" +
+            "CMD: 'show history all | include Configured from' 08:58:03 UTC Tue Apr 24 2018\n" +
+            "CMD: 'show history all | include Configured from | LINE 2' 09:03:05 UTC Tue Apr 24 2018\n" +
+            "CMD: 'show history all | include Configured from |' 09:05:36 UTC Tue Apr 24 2018\n" +
+            "CMD: 'show history all | include \"Configured from\"' 09:06:04 UTC Tue Apr 24 2018\n" +
+            "CMD: 'show history all | include Configured from' 09:12:48 UTC Tue Apr 24 2018\n" +
+            "CMD: 'show history all | include Configured from' 10:49:48 UTC Tue Apr 24 2018\n";
+
+    private static final String EXPECTED = "Apr 23 14:00:01.896";
+
+    @Test
+    public void testParse() {
+
+        Optional<String> date1 = ConfigMetadataReader.getLastConfigurationFingerprint(OUTPUT);
+        Assert.assertTrue(date1.isPresent());
+        Assert.assertEquals(EXPECTED, date1.get());
+
+        Optional<String> date2 = (ConfigMetadataReader.getLastConfigurationFingerprint(""));
+        assertFalse(date2.isPresent());
+    }
+}
