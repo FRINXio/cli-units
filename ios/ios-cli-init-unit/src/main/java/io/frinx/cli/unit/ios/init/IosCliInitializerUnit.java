d2ab3f72276e9584ed166dae9f2d520b5d91171e
Maros Marsalek <mmarsalek@frinx.io>
2018-06-06 17:35:49 +0200

    Speedup IOS initializer unit
    Change-Id: Ia7809a22a0fa0023b49cbfc776be2c0102e70a5b
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- ios/ios-cli-init-unit/src/main/java/io/frinx/cli/unit/ios/init/IosCliInitializerUnit.java
+++ ios/ios-cli-init-unit/src/main/java/io/frinx/cli/unit/ios/init/IosCliInitializerUnit.java
@@ -151,20 +151,23 @@ public class IosCliInitializerUnit  implements TranslateUnit {
         @Override
         public void accept(@Nonnull Session session, @Nonnull String newline) {
             try {
+                String initialPrompt = PromptResolutionStrategy.ENTER_AND_READ.resolvePrompt(session, newline).trim();
 
                 // Set terminal length to 0 to prevent "--More--" situation
                 LOG.debug("{}: Setting terminal length to 0 to prevent \"--More--\" situation", id);
                 write(session, newline, SET_TERMINAL_LENGTH_COMMAND);
-                session.readUntilTimeout(READ_TIMEOUT_SECONDS);
 
                 // Set terminal width to 0 to prevent command shortening
                 LOG.debug("{}: Setting terminal width to 0", id);
                 write(session, newline, SET_TERMINAL_WIDTH_COMMAND);
-                session.readUntilTimeout(READ_TIMEOUT_SECONDS);
 
+                String initOutput = session.readUntilOutput(initialPrompt)
+                        .toCompletableFuture()
+                        .get();
+
+                LOG.debug("{}: IOS-XR cli session initialized output: {}", id, initOutput);
                 // If already in privileged mode, don't do anything else
-                if (PromptResolutionStrategy.ENTER_AND_READ.resolvePrompt(session, newline).trim()
-                        .endsWith(PRIVILEGED_PROMPT_SUFFIX)) {
+                if (initialPrompt.endsWith(PRIVILEGED_PROMPT_SUFFIX)) {
                     LOG.info("{}: IOS cli session initialized successfully", id);
                     return;
                 }
