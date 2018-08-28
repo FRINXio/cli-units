0b4409a2582c95bd7eb1cd6c6198f21af21f14b7
kensho.ri <kensho.ri@g.softbank.co.jp>
2018-08-28 19:10:18 +0900

    Added EthernetConfigReader and BundleEtherLacpMemberConfigReader
    Change-Id: I37846d1028991b0c83ee85c4a27fbb7036698302
    Signed-off-by: kenshori <kensho.ri@g.softbank.co.jp>
    (cherry picked from commit 27cabf79280ec9e364543eff97b60940cba37245)

---
+++ dasan/interface/src/main/java/io/frinx/cli/unit/dasan/ifc/handler/ethernet/EthernetConfigReader.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2018 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.unit.dasan.ifc.handler.ethernet;
+
+import io.fd.honeycomb.translate.spi.read.ReaderCustomizer;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.registry.common.CompositeReader;
+import io.frinx.cli.unit.dasan.ifc.handler.ethernet.lacpmember.BundleEtherLacpMemberConfigReader;
+import io.frinx.cli.unit.utils.CliConfigReader;
+import java.util.ArrayList;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.ethernet.rev161222.ethernet.top.EthernetBuilder;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.ethernet.rev161222.ethernet.top.ethernet.Config;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.ethernet.rev161222.ethernet.top.ethernet.ConfigBuilder;
+import org.opendaylight.yangtools.concepts.Builder;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public final class EthernetConfigReader extends CompositeReader<Config, ConfigBuilder>
+        implements CliConfigReader<Config, ConfigBuilder> {
+
+    public EthernetConfigReader(Cli cli) {
+        super(new ArrayList<ReaderCustomizer<Config, ConfigBuilder>>() {
+            {
+                add(new BundleEtherLacpMemberConfigReader(cli));
+            }
+        });
+    }
+
+    @Nonnull
+    @Override
+    public ConfigBuilder getBuilder(@Nonnull InstanceIdentifier<Config> instanceIdentifier) {
+        return new ConfigBuilder();
+    }
+
+    @Override
+    public void merge(@Nonnull final Builder<? extends DataObject> builder, @Nonnull final Config value) {
+        ((EthernetBuilder) builder).setConfig(value);
+    }
+}
