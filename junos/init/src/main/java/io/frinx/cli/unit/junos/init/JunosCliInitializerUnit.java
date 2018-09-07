ba309157d7edd096bdce86d80ebcbe4534484cdf
isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
2018-09-07 15:30:54 +0900

    FRHD-274: Updated JunosCliInitializerUnit.
    Change-Id: I0a3b3601290d05baadec4c8240d3e576ebfd6c02
    Signed-off-by: isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>

--- junos/init/src/main/java/io/frinx/cli/unit/junos/init/JunosCliInitializerUnit.java
+++ junos/init/src/main/java/io/frinx/cli/unit/junos/init/JunosCliInitializerUnit.java
@@ -16,23 +16,31 @@
 
 package io.frinx.cli.unit.junos.init;
 
-import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
 import io.fd.honeycomb.rpc.RpcService;
 import io.fd.honeycomb.translate.read.registry.ModifiableReaderRegistryBuilder;
+import io.fd.honeycomb.translate.spi.write.CommitFailedException;
+import io.fd.honeycomb.translate.spi.write.PostCommitHook;
+import io.fd.honeycomb.translate.spi.write.PostFailedHook;
+import io.fd.honeycomb.translate.spi.write.PreCommitHook;
 import io.fd.honeycomb.translate.write.registry.ModifiableWriterRegistryBuilder;
+import io.fd.honeycomb.translate.write.registry.WriterRegistry;
+import io.frinx.cli.io.Cli;
 import io.frinx.cli.io.PromptResolutionStrategy;
-import io.frinx.cli.io.Session;
-import io.frinx.cli.io.SessionException;
 import io.frinx.cli.io.SessionInitializationStrategy;
 import io.frinx.cli.registry.api.TranslationUnitCollector;
 import io.frinx.cli.registry.spi.TranslateUnit;
 import io.frinx.cli.topology.RemoteDeviceId;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
+import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.topology.rev170520.CliNode;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.Device;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.DeviceIdBuilder;
@@ -48,6 +56,11 @@ public class JunosCliInitializerUnit implements TranslateUnit {
             .setDeviceType("junos")
             .setDeviceVersion("*")
             .build();
+
+    private static final PromptResolutionStrategy PROMPT_RESOLUTION_STRATEGY =
+        JunosPromptResolutionStrategy.getInstance();
+
+    private static JunosCliInitializationStrategy INITIALIZER = new JunosCliInitializationStrategy();
 
     private TranslationUnitCollector registry;
     private TranslationUnitCollector.Registration reg;
@@ -72,67 +85,6 @@ public class JunosCliInitializerUnit implements TranslateUnit {
     }
 
     @Override
-    public SessionInitializationStrategy getInitializer(@Nonnull final RemoteDeviceId id,
-                                                        @Nonnull final CliNode cliNodeConfiguration) {
-        return new SessionInitializationStrategy() {
-
-            private static final String PRIVILEGED_PROMPT_SUFFIX = ">";
-            private static final String CLI_COMMAND = "cli";
-            private static final String DISABLE_COMPLETE_ON_SPACE = "set cli complete-on-space off";
-            private static final String SET_TERMINAL_LENGTH_COMMAND = "set cli screen-length 0";
-            private static final String SET_TERMINAL_WIDTH_COMMAND = "set cli screen-width 0";
-            private static final int READ_TIMEOUT_SECONDS = 1;
-
-            @Override
-            public void accept(Session session, String newline) {
-                try {
-                    // enable cli mode
-                    tryToEnterCliMode(session, newline);
-
-                    // Check if we are actually in cli mode
-                    String prompt = PromptResolutionStrategy.ENTER_AND_READ.resolvePrompt(session, newline)
-                            .trim();
-                    // If not, fail
-                    Preconditions.checkState(prompt.endsWith(PRIVILEGED_PROMPT_SUFFIX),
-                            "%s: Junos cli session initialization failed to enter cli mode. Current prompt: %s", id,
-                            prompt);
-
-                    LOG.debug("{}: Setting terminal complete command on space to off", id);
-                    write(session, newline, DISABLE_COMPLETE_ON_SPACE);
-                    session.readUntilTimeout(READ_TIMEOUT_SECONDS);
-
-                    LOG.debug("{}: Setting terminal length to 0 to prevent \"--More--\" situation", id);
-                    write(session, newline, SET_TERMINAL_LENGTH_COMMAND);
-                    session.readUntilTimeout(READ_TIMEOUT_SECONDS);
-
-                    LOG.debug("{}: Setting terminal width to 0", id);
-                    write(session, newline, SET_TERMINAL_WIDTH_COMMAND);
-                    session.readUntilTimeout(READ_TIMEOUT_SECONDS);
-
-                    LOG.info("{}: Junos cli session initialized successfully", id);
-                } catch (InterruptedException e) {
-                    Thread.currentThread()
-                            .interrupt();
-                    throw new RuntimeException(e);
-                } catch (SessionException | ExecutionException | TimeoutException e) {
-                    LOG.warn("{}: Unable to initialize device", id, e);
-                    throw new IllegalStateException(id + ": Unable to initialize device", e);
-                }
-
-            }
-
-            private void tryToEnterCliMode(@Nonnull Session session, @Nonnull String newline)
-                    throws InterruptedException, ExecutionException, TimeoutException {
-
-                write(session, newline, CLI_COMMAND);
-                String cliCommandOutput = session.readUntilTimeout(READ_TIMEOUT_SECONDS)
-                        .trim();
-                LOG.debug("{}: Entering cli mode resulted in output: {}", id, cliCommandOutput);
-            }
-        };
-    }
-
-    @Override
     public Set<RpcService<?, ?>> getRpcs(@Nonnull final TranslateUnit.Context context) {
         return Sets.newHashSet();
     }
@@ -148,5 +100,100 @@ public class JunosCliInitializerUnit implements TranslateUnit {
     public String toString() {
         return "Junos cli init (FRINX) translate unit";
     }
-}
 
+    @Override
+    public SessionInitializationStrategy getInitializer(@Nonnull final RemoteDeviceId id,
+                                                        @Nonnull final CliNode cliNodeConfiguration) {
+        return INITIALIZER;
+    }
+
+    @Override
+    public PreCommitHook getPreCommitHook(Context context) {
+        return () -> {
+            Cli cli = context.getTransport();
+            try {
+                JunosCliInitializationStrategy.tryToEnterConfigurationMode(cli);
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                LOG.warn("{}: Unable to enter configuration mode", cli, e);
+                throw new IllegalStateException(cli + ": Unable to enter configuration mode", e);
+            }
+        };
+    }
+
+    @Override
+    @SuppressWarnings({"IllegalCatch", "AvoidHidingCauseException"})
+    public PostCommitHook getCommitHook(Context context, Set<Pattern> errorCommitPatterns) {
+        return () -> {
+            Cli cli = null;
+            try {
+                cli = context.getTransport();
+                JunosCliInitializationStrategy.tryToCommit(cli, errorCommitPatterns);
+                LOG.debug("{}: Commit successful", cli);
+            } catch (Exception e) {
+                LOG.warn("{}: Commit failed", cli, e);
+                throw new CommitFailedException(cli + ": Commit failed");
+            }
+
+            try {
+                JunosCliInitializationStrategy.tryToExitConfigurationMode(cli, false);
+                LOG.debug("{}: Exit configuration mode successful", cli);
+            } catch (Exception e) {
+                LOG.warn("{}: Unable to exit configuration mode", cli, e);
+                throw new IllegalStateException(cli + ": Unable to exit configuration mode", e);
+            }
+        };
+    }
+
+    @Override
+    public PostFailedHook getPostFailedHook(Context context) {
+        return (causingException) -> {
+            Cli cli = context.getTransport();
+            String message = Optional.ofNullable(causingException)
+                    .map(JunosCliInitializerUnit::getBottomErrorMessage)
+                    .orElse("Unknown reason.");
+            LOG.warn("{}: Configuration failed: {}", cli, message);
+
+            try {
+                JunosCliInitializationStrategy.tryToExitConfigurationMode(cli, true);
+
+                LOG.debug("{}: Revert successful.", cli);
+                throw new WriterRegistry.Reverter.RevertSuccessException(message);
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                LOG.warn("{}: Failed to abort commit. Reason: {}", cli, e.getMessage(), e);
+                throw new WriterRegistry.Reverter.RevertFailedException(e);
+            }
+        };
+    }
+
+    @Nullable
+    private static String getBottomErrorMessage(Throwable throwable) {
+        Throwable cause = throwable;
+        while (cause.getCause() != null) {
+            cause = cause.getCause();
+        }
+        return Strings.emptyToNull(cause.getMessage());
+    }
+
+    @Override
+    public Set<Pattern> getErrorPatterns() {
+        return Sets.newLinkedHashSet(Arrays.asList(
+                Pattern.compile("(^|\\n)\\s+\\^.*", Pattern.DOTALL),
+                Pattern.compile("(^|\\n)(?i)unknown command\\.(?-i).*", Pattern.DOTALL),
+                Pattern.compile("(^|\\n)(?i)syntax error,(?-i) .*", Pattern.DOTALL),
+                Pattern.compile("(^|\\n)(?i)'.+' is ambiguous\\.(?-i).*", Pattern.DOTALL),
+                Pattern.compile("(^|\\n)(?i)error:(?-i) .*", Pattern.DOTALL)
+        ));
+    }
+
+    @Override
+    public Set<Pattern> getCommitErrorPatterns() {
+        return Sets.newLinkedHashSet(Arrays.asList(
+                Pattern.compile("(^|\\n)(?i)error:(?-i) .*", Pattern.DOTALL)
+            ));
+    }
+
+    @Override
+    public PromptResolutionStrategy getPromptResolver() {
+        return PROMPT_RESOLUTION_STRATEGY;
+    }
+}
