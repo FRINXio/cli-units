361bd7bcb1a3cd0c604bc19a2d102bb97e314b01
Dana Kutenicsova <dkutenicsova@frinx.io>
2018-10-10 11:04:45 +0200

    FI-482 : switched huawei units to use IIDs generator
    Change-Id: Ife3537bda95abe3c2ef00c63ae465a9ee9d46ce1
    Signed-off-by: Dana Kutenicsova <dkutenicsova@frinx.io>

--- huawei/routing-policy/src/main/java/io/frinx/cli/unit/huawei/routing/policy/RoutingPolicyUnit.java
+++ huawei/routing-policy/src/main/java/io/frinx/cli/unit/huawei/routing/policy/RoutingPolicyUnit.java
@@ -32,20 +32,14 @@ import io.frinx.openconfig.openconfig.policy.IIDs;
 import java.util.Collections;
 import java.util.Set;
 import javax.annotation.Nonnull;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.policy.rev170730.DefinedSets2;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.policy.rev170730.DefinedSets2Builder;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.policy.rev170730.ext.community.set.top.ExtCommunitySets;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.policy.rev170730.ext.community.set.top.ExtCommunitySetsBuilder;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.policy.rev170730.ext.community.set.top.ext.community.sets.ExtCommunitySet;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.policy.rev170730.ext.community.set.top.ext.community.sets.ext.community.set.Config;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.policy.rev170730.routing.policy.defined.sets.BgpDefinedSets;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.policy.rev170730.routing.policy.defined.sets.BgpDefinedSetsBuilder;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.$YangModuleInfoImpl;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.defined.sets.top.DefinedSetsBuilder;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.routing.policy.top.RoutingPolicyBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.Device;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.DeviceIdBuilder;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
 
 public class RoutingPolicyUnit implements TranslateUnit {
@@ -54,16 +48,6 @@ public class RoutingPolicyUnit implements TranslateUnit {
             .setDeviceType("vrp")
             .setDeviceVersion("*")
             .build();
-
-    private static final InstanceIdentifier<DefinedSets2> DEFINED_SETS_1 =
-            IIDs.RO_DEFINEDSETS.augmentation(DefinedSets2.class);
-    private static final InstanceIdentifier<BgpDefinedSets> BGP_DEFINED_SETS =
-            DEFINED_SETS_1.child(BgpDefinedSets.class);
-    private static final InstanceIdentifier<ExtCommunitySets> EXT_COMMUNITY_SETS =
-            BGP_DEFINED_SETS.child(ExtCommunitySets.class);
-    private static final InstanceIdentifier<ExtCommunitySet> EXT_COMMUNITY_SET =
-            EXT_COMMUNITY_SETS.child(ExtCommunitySet.class);
-    private static final InstanceIdentifier<Config> EXT_CS_CONFIG = EXT_COMMUNITY_SET.child(Config.class);
 
     private final TranslationUnitCollector registry;
     private TranslationUnitCollector.Registration reg;
@@ -100,11 +84,20 @@ public class RoutingPolicyUnit implements TranslateUnit {
         // provide writers
         writeRegistry.add(new GenericWriter<>(IIDs.ROUTINGPOLICY, new NoopCliWriter<>()));
         writeRegistry.add(new GenericWriter<>(IIDs.RO_DEFINEDSETS, new NoopCliWriter<>()));
-        writeRegistry.add(new GenericWriter<>(DEFINED_SETS_1, new NoopCliWriter<>()));
-        writeRegistry.add(new GenericWriter<>(BGP_DEFINED_SETS, new NoopCliWriter<>()));
-        writeRegistry.add(new GenericWriter<>(EXT_COMMUNITY_SETS, new NoopCliWriter<>()));
-        writeRegistry.add(new GenericWriter<>(EXT_COMMUNITY_SET, new NoopCliWriter<>()));
-        writeRegistry.addAfter(new GenericWriter<>(EXT_CS_CONFIG,new ExtCommunitySetConfigWriter(cli)),
+        writeRegistry.add(new GenericWriter<>(io.frinx.openconfig.openconfig.bgp.IIDs.RO_DE_AUG_DEFINEDSETS2,
+                new NoopCliWriter<>()));
+        writeRegistry.add(new GenericWriter<>(
+                io.frinx.openconfig.openconfig.bgp.IIDs.RO_DE_AUG_DEFINEDSETS2_BGPDEFINEDSETS,
+                new NoopCliWriter<>()));
+        writeRegistry.add(new GenericWriter<>(
+                io.frinx.openconfig.openconfig.bgp.IIDs.RO_DE_AUG_DEFINEDSETS2_BG_EXTCOMMUNITYSETS,
+                new NoopCliWriter<>()));
+        writeRegistry.add(new GenericWriter<>(
+                io.frinx.openconfig.openconfig.bgp.IIDs.RO_DE_AUG_DEFINEDSETS2_BG_EX_EXTCOMMUNITYSET,
+                new NoopCliWriter<>()));
+        writeRegistry.addAfter(new GenericWriter<>(
+                io.frinx.openconfig.openconfig.bgp.IIDs.RO_DE_AUG_DEFINEDSETS2_BG_EX_EX_CONFIG,
+                        new ExtCommunitySetConfigWriter(cli)),
                 io.frinx.openconfig.openconfig.network.instance.IIDs.NE_NE_CONFIG);
     }
 
@@ -112,10 +105,16 @@ public class RoutingPolicyUnit implements TranslateUnit {
         // provide readers
         readRegistry.addStructuralReader(IIDs.ROUTINGPOLICY, RoutingPolicyBuilder.class);
         readRegistry.addStructuralReader(IIDs.RO_DEFINEDSETS, DefinedSetsBuilder.class);
-        readRegistry.addStructuralReader(DEFINED_SETS_1, DefinedSets2Builder.class);
-        readRegistry.addStructuralReader(BGP_DEFINED_SETS, BgpDefinedSetsBuilder.class);
-        readRegistry.addStructuralReader(EXT_COMMUNITY_SETS, ExtCommunitySetsBuilder.class);
-        readRegistry.add(new GenericConfigListReader<>(EXT_COMMUNITY_SET, new ExtCommunitySetReader(cli)));
+        readRegistry.addStructuralReader(io.frinx.openconfig.openconfig.bgp.IIDs.RO_DE_AUG_DEFINEDSETS2,
+                DefinedSets2Builder.class);
+        readRegistry.addStructuralReader(io.frinx.openconfig.openconfig.bgp.IIDs.RO_DE_AUG_DEFINEDSETS2_BGPDEFINEDSETS,
+                BgpDefinedSetsBuilder.class);
+        readRegistry.addStructuralReader(
+                io.frinx.openconfig.openconfig.bgp.IIDs.RO_DE_AUG_DEFINEDSETS2_BG_EXTCOMMUNITYSETS,
+                ExtCommunitySetsBuilder.class);
+        readRegistry.add(new GenericConfigListReader<>(
+                io.frinx.openconfig.openconfig.bgp.IIDs.RO_DE_AUG_DEFINEDSETS2_BG_EX_EXTCOMMUNITYSET,
+                new ExtCommunitySetReader(cli)));
     }
 
     @Override
