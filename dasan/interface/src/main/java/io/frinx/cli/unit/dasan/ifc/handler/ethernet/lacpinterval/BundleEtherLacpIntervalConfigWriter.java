69bf94f674ad0405d2a3adcf70a505b417074f85
tatsuso.cho <tatsuso.cho@g.softbank.co.jp>
2019-01-20 17:49:17 +0900

    Add "lacp port timeout" command to DASAN.
    Change-Id: I23d17880b533b132126a29ed208a9620c4af9bfe
    Signed-off-by: tatsuso.cho <tatsuso.cho@g.softbank.co.jp>
    (cherry picked from commit 234900fed63c87ae2bafddeeac5b47b4cbae0023)

---
+++ dasan/interface/src/main/java/io/frinx/cli/unit/dasan/ifc/handler/ethernet/lacpinterval/BundleEtherLacpIntervalConfigWriter.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.unit.dasan.ifc.handler.ethernet.lacpinterval;
+
+import io.fd.honeycomb.translate.write.WriteContext;
+import io.fd.honeycomb.translate.write.WriteFailedException;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.unit.utils.CliWriter;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.ethernet.rev161222.ethernet.top.ethernet.Config;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.interfaces.Interface;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.lacp.lag.member.rev171109.LacpEthConfigAug;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.lacp.rev170505.LacpPeriodType;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class BundleEtherLacpIntervalConfigWriter implements CliWriter<Config> {
+
+    private final Cli cli;
+
+    public BundleEtherLacpIntervalConfigWriter(Cli cli) {
+        this.cli = cli;
+    }
+
+    @Override
+    public void writeCurrentAttributes(@Nonnull InstanceIdentifier<Config> id, @Nonnull Config dataAfter,
+            @Nonnull WriteContext writeContext) throws WriteFailedException {
+        String ifcName = id.firstKeyOf(Interface.class).getName();
+        LacpEthConfigAug cfg1 = dataAfter.getAugmentation(LacpEthConfigAug.class);
+
+        if (cfg1 == null
+                || cfg1.getInterval() == null
+                || !cfg1.getInterval().equals(LacpPeriodType.FAST)) { //only support FAST know
+            return;
+        }
+
+        blockingWriteAndRead(cli, id, dataAfter, "configure terminal", "bridge",
+                f("lacp port timeout %s short", ifcName.replace("Ethernet", "")), "end");
+    }
+
+    @Override
+    public void deleteCurrentAttributes(@Nonnull InstanceIdentifier<Config> id, @Nonnull Config dataBefore,
+            @Nonnull WriteContext writeContext) throws WriteFailedException {
+
+        String ifcName = id.firstKeyOf(Interface.class).getName();
+        LacpEthConfigAug cfg1 = dataBefore.getAugmentation(LacpEthConfigAug.class);
+
+        if (cfg1 == null || cfg1.getInterval() == null) {
+            return;
+        }
+
+        blockingDeleteAndRead(cli, id, "configure terminal", "bridge",
+                f("no lacp port timeout %s", ifcName.replace("Ethernet", "")), "end");
+    }
+}
