9bc2a4bde74617b9b0cf219d485cbc6558412676
Maros Marsalek <mmarsalek@frinx.io>
2019-06-11 10:36:52 +0200

    Fix ios bgp neighbor config reader command
    The command used:
    show running-config | include ^router bgp|^ *address-family|^ *neighbor 111.72.20.1
    
    could return:
    router bgp 64496
     neighbor 111.72.20.1 peer-group AG-GROUP
     neighbor 111.72.20.1 description CB200-077AG-11
     neighbor 111.72.20.129 peer-group AG-GROUP
     neighbor 111.72.20.129 description CB200-024AG-12
     address-family ipv4
      neighbor 111.72.20.1 activate
      neighbor 111.72.20.129 activate
    
    Change-Id: Ibabf87c55bc3f37f35ebedd2a46f89dcbd8ad1df
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- ios/bgp/src/main/java/io/frinx/cli/ios/bgp/handler/neighbor/NeighborConfigReader.java
+++ ios/bgp/src/main/java/io/frinx/cli/ios/bgp/handler/neighbor/NeighborConfigReader.java
@@ -42,7 +42,7 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 public class NeighborConfigReader implements CliConfigReader<Config, ConfigBuilder> {
 
-    public static final String SH_SUMM = "show running-config | include ^router bgp|^ *address-family|^ *neighbor %s";
+    public static final String SH_SUMM = "show running-config | include ^router bgp|^ *address-family|^ *neighbor %s ";
     private static final String PASSWORD_FORM = "Encrypted[%s]";
 
     private static final Pattern NEIGHBOR_ACTIVATE_PATTERN =
