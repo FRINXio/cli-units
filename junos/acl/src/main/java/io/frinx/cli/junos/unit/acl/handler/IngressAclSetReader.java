5b2ed11a199acc8ae1a20f7a5326f1d1509caed9
simonmisencik <smisencik@frinx.io>
2019-06-13 10:34:49 +0200

    Refactoring junos acl unit after FI-783
    Signed-off-by: simonmisencik <smisencik@frinx.io>
    Change-Id: If8892f09beaa6a706aeb11a944e7bb22eec2d65b

--- junos/acl/src/main/java/io/frinx/cli/junos/unit/acl/handler/IngressAclSetReader.java
+++ junos/acl/src/main/java/io/frinx/cli/junos/unit/acl/handler/IngressAclSetReader.java
@@ -26,13 +26,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526.ACLIPV4;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526._interface.ingress.acl.top.IngressAclSetsBuilder;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526._interface.ingress.acl.top.ingress.acl.sets.IngressAclSet;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526._interface.ingress.acl.top.ingress.acl.sets.IngressAclSetBuilder;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526._interface.ingress.acl.top.ingress.acl.sets.IngressAclSetKey;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.acl.rev170526.acl.interfaces.top.interfaces.Interface;
-import org.opendaylight.yangtools.concepts.Builder;
-import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 public class IngressAclSetReader
@@ -67,11 +64,6 @@ public class IngressAclSetReader
             matcher.group("unit"),
             instanceIdentifier,
             readContext);
-    }
-
-    @Override
-    public void merge(@Nonnull Builder<? extends DataObject> builder, @Nonnull List<IngressAclSet> list) {
-        ((IngressAclSetsBuilder) builder).setIngressAclSet(list);
     }
 
     @Override
