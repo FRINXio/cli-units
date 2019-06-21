77388a5bcf0b52fd8641a09181d44e8679c28216
tatsuso.cho <tatsuso.cho@g.softbank.co.jp>
2019-06-21 13:52:35 +0900

    Added evpn project.
    Signed-off-by: tatsuso.cho <tatsuso.cho@g.softbank.co.jp>
    Change-Id: Iada4c0d20607609d79790942e046bb5322e11e3f

---
+++ ios-xr/evpn/src/main/java/io/frinx/cli/iosxr/evpn/EvpnUnit.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.iosxr.evpn;
+
+import com.google.common.collect.Sets;
+import io.fd.honeycomb.translate.spi.builder.CustomizerAwareReadRegistryBuilder;
+import io.fd.honeycomb.translate.spi.builder.CustomizerAwareWriteRegistryBuilder;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.iosxr.evpn.handler.EvpnConfigReader;
+import io.frinx.cli.iosxr.evpn.handler.EvpnConfigWriter;
+import io.frinx.cli.registry.api.TranslationUnitCollector;
+import io.frinx.cli.unit.iosxr.init.IosXrDevices;
+import io.frinx.cli.unit.utils.AbstractUnit;
+import io.frinx.openconfig.openconfig.evpn.IIDs;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.cli.translate.registry.rev170520.Device;
+import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
+
+public class EvpnUnit extends AbstractUnit {
+
+    public EvpnUnit(@Nonnull final TranslationUnitCollector registry) {
+        super(registry);
+    }
+
+    @Override
+    protected Set<Device> getSupportedVersions() {
+        return IosXrDevices.IOS_XR_ALL;
+    }
+
+    @Override
+    protected String getUnitName() {
+        return "IOS XR Evpn unit";
+    }
+
+    @Override
+    public Set<YangModuleInfo> getYangSchemas() {
+        return Sets.newHashSet(IIDs.FRINX_EVPN);
+    }
+
+    @Override
+    public void provideHandlers(@Nonnull final CustomizerAwareReadRegistryBuilder readRegistry,
+            @Nonnull final CustomizerAwareWriteRegistryBuilder writeRegistry, @Nonnull final Context context) {
+        Cli cli = context.getTransport();
+        provideReaders(readRegistry, cli);
+        provideWriters(writeRegistry, cli);
+    }
+
+    private void provideWriters(CustomizerAwareWriteRegistryBuilder writeRegistry, Cli cli) {
+        writeRegistry.addNoop(IIDs.EVPN);
+        writeRegistry.add(IIDs.EV_CONFIG, new EvpnConfigWriter(cli));
+    }
+
+    private void provideReaders(@Nonnull CustomizerAwareReadRegistryBuilder readRegistry, Cli cli) {
+        readRegistry.add(IIDs.EV_CONFIG, new EvpnConfigReader(cli));
+    }
+}
