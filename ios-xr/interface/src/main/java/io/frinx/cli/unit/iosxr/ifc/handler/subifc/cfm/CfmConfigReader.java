0cbfbda11b371b0c6b4aa36e60bcc78b7966351b
isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
2019-07-19 15:01:09 +0900

    Added cfm config handlers to ios-xr-interface-unit.
    Change-Id: I4eaecdd7e533d5a5cf29b13fe650c8ac04b992ba
    Signed-off-by: isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
    (cherry picked from commit f1fa854fed3c7af987ea5c9e504981e9bb98a718)

---
+++ ios-xr/interface/src/main/java/io/frinx/cli/unit/iosxr/ifc/handler/subifc/cfm/CfmConfigReader.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.unit.iosxr.ifc.handler.subifc.cfm;
+
+import io.fd.honeycomb.translate.read.ReadContext;
+import io.fd.honeycomb.translate.read.ReadFailedException;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.unit.iosxr.ifc.Util;
+import io.frinx.cli.unit.iosxr.ifc.handler.InterfaceConfigReader;
+import io.frinx.cli.unit.iosxr.ifc.handler.aggregate.AggregateConfigReader;
+import io.frinx.cli.unit.utils.CliConfigReader;
+import io.frinx.cli.unit.utils.ParsingUtils;
+import java.util.regex.Pattern;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.oam.rev190619.ethernet.cfm._interface.cfm.Config;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.oam.rev190619.ethernet.cfm._interface.cfm.ConfigBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class CfmConfigReader implements CliConfigReader<Config, ConfigBuilder> {
+    private final Cli cli;
+
+    public CfmConfigReader(Cli cli) {
+        this.cli = cli;
+    }
+
+    private static final Pattern CFM_ENABLED = Pattern.compile("ethernet cfm");
+
+    @Override
+    public void readCurrentAttributes(
+        @Nonnull InstanceIdentifier<Config> id,
+        @Nonnull ConfigBuilder builder,
+        @Nonnull ReadContext ctx) throws ReadFailedException {
+
+        String ifcName = Util.getSubinterfaceName(id);
+        if (!new AggregateConfigReader(cli).isLAGInterface(ifcName)) {
+            // read cfm configuration just for LAG interfaces
+            return;
+        }
+
+        String output = blockingRead(f(InterfaceConfigReader.SH_SINGLE_INTERFACE_CFG, ifcName), cli, id, ctx);
+        parseCfmConfig(output, builder);
+    }
+
+    private static void parseCfmConfig(String output, ConfigBuilder builder) {
+        ParsingUtils.parseField(output,
+            CFM_ENABLED::matcher,
+            matcher -> Boolean.TRUE,
+            builder::setEnabled);
+    }
+}
---
+++ ios-xr/interface/src/test/java/io/frinx/cli/unit/iosxr/ifc/handler/subifc/cfm/CfmConfigWriterTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright © 2019 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.unit.iosxr.ifc.handler.subifc.cfm;
+
+import io.fd.honeycomb.translate.write.WriteContext;
+import io.fd.honeycomb.translate.write.WriteFailedException;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.io.Command;
+import io.frinx.openconfig.openconfig.oam.IIDs;
+import java.util.concurrent.CompletableFuture;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.interfaces.InterfaceKey;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.subinterfaces.top.subinterfaces.SubinterfaceKey;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.oam.rev190619.ethernet.cfm._interface.cfm.Config;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.oam.rev190619.ethernet.cfm._interface.cfm.ConfigBuilder;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.utils.IidUtils;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class CfmConfigWriterTest {
+    @Mock
+    private Cli cli;
+    @Mock
+    private WriteContext writeContext;
+    private CfmConfigWriter target;
+    private ArgumentCaptor<Command> response = ArgumentCaptor.forClass(Command.class);
+
+    private static final String WRITE_INPUT = "interface Bundle-Ether1000.100\n"
+        + "ethernet cfm\n"
+        + "root\n";
+    private static final String DELETE_INPUT = "interface Bundle-Ether1000.100\n"
+        + "no ethernet cfm\n"
+        + "root\n";
+
+    private static final String INTERFACE_NAME = "Bundle-Ether1000";
+    private static final Long SUBIFC_INDEX = Long.valueOf(100L);
+
+    private static final InterfaceKey INTERFACE_KEY = new InterfaceKey(INTERFACE_NAME);
+    private static final SubinterfaceKey SUBIFC_KEY = new SubinterfaceKey(SUBIFC_INDEX);
+    private static final InstanceIdentifier<Config> IID = IidUtils.createIid(
+        IIDs.IN_IN_SU_SU_AUG_IFSUBIFCFMAUG_CF_CONFIG,
+        INTERFACE_KEY, SUBIFC_KEY);
+
+    private static final Config DATA_ENABLED = new ConfigBuilder()
+        .setEnabled(Boolean.TRUE)
+        .build();
+
+    private static final Config DATA_DISABLED = new ConfigBuilder()
+        .setEnabled(Boolean.FALSE)
+        .build();
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(cli.executeAndRead(Mockito.any())).then(invocation -> CompletableFuture.completedFuture(""));
+        target = new CfmConfigWriter(cli);
+    }
+
+    @Test
+    public void testWriteCurrentAttributesEnabled() throws WriteFailedException {
+        target.writeCurrentAttributes(IID, DATA_ENABLED, writeContext);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(WRITE_INPUT, response.getValue().getContent());
+    }
+
+    @Test
+    public void testWriteCurrentAttributesDisabled() throws WriteFailedException {
+        target.writeCurrentAttributes(IID, DATA_DISABLED, writeContext);
+        Mockito.verify(cli, Mockito.never()).executeAndRead(Mockito.any());
+    }
+
+    @Test
+    public void testUpdateCurrentAttributesDisabled() throws WriteFailedException {
+        target.updateCurrentAttributes(IID, DATA_ENABLED, DATA_DISABLED, writeContext);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(DELETE_INPUT, response.getValue().getContent());
+    }
+
+    @Test
+    public void testUpdateCurrentAttributesEnabled() throws WriteFailedException {
+        target.updateCurrentAttributes(IID, DATA_DISABLED, DATA_ENABLED, writeContext);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(WRITE_INPUT, response.getValue().getContent());
+    }
+
+    @Test
+    public void testDeleteCurrentAttributesEnabled() throws WriteFailedException {
+        target.deleteCurrentAttributes(IID, DATA_ENABLED, writeContext);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(DELETE_INPUT, response.getValue().getContent());
+    }
+
+    @Test
+    public void testDeleteCurrentAttributesDisabled() throws WriteFailedException {
+        target.deleteCurrentAttributes(IID, DATA_DISABLED, writeContext);
+        Mockito.verify(cli, Mockito.never()).executeAndRead(Mockito.any());
+    }
+}
