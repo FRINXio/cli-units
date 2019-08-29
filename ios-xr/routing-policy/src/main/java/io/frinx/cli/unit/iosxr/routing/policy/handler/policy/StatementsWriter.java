f8fdf0e1af78948e237d82ad1b08ddae2952cbf3
isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>
2019-08-29 18:35:57 +0900

    Modified statements handlers to use ios-xr-route-policy-util.
    Change-Id: I3e71eb982dd4f8da6906379a56f8be58fdb24a8c
    Signed-off-by: isamu.kawaguchi <isamu.kawaguchi04@g.softbank.co.jp>

--- ios-xr/routing-policy/src/main/java/io/frinx/cli/unit/iosxr/routing/policy/handler/policy/StatementsWriter.java
+++ ios-xr/routing-policy/src/main/java/io/frinx/cli/unit/iosxr/routing/policy/handler/policy/StatementsWriter.java
@@ -16,21 +16,13 @@
 
 package io.frinx.cli.unit.iosxr.routing.policy.handler.policy;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
 import io.fd.honeycomb.translate.write.WriteContext;
 import io.fd.honeycomb.translate.write.WriteFailedException;
 import io.frinx.cli.io.Cli;
-import io.frinx.cli.unit.utils.CliFormatter;
 import io.frinx.cli.unit.utils.CliWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import io.frinx.translate.unit.iosxr.route.policy.util.StatementsRenderer;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import javax.annotation.Nonnull;
-import org.apache.commons.lang3.math.NumberUtils;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.policy.definitions.top.policy.definitions.PolicyDefinition;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.policy.statements.top.Statements;
 import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.policy.statements.top.statements.Statement;
@@ -49,85 +41,10 @@ public class StatementsWriter implements CliWriter<Statements> {
                                        @Nonnull Statements dataAfter,
                                        @Nonnull WriteContext writeContext) throws WriteFailedException {
         List<Statement> statements = dataAfter.getStatement();
-        if (statements == null) {
-            return;
-        }
-
-        List<Statement> sortedStatements = sortByNames(dataAfter.getStatement());
-        validateNames(sortedStatements);
 
         String rpName = id.firstKeyOf(PolicyDefinition.class).getName();
         blockingWriteAndRead(cli, id, dataAfter,
-                processStatements(sortedStatements, rpName, this));
-    }
-
-    @VisibleForTesting
-    static List<Statement> sortByNames(List<Statement> statements) {
-        return statements.stream()
-                .peek(s -> Preconditions.checkArgument(NumberUtils.isCreatable(s.getName()),
-                        "Statement name must be a number but was " + s.getName()))
-                .sorted(Comparator.comparingInt(s -> Integer.valueOf(s.getName())))
-                .collect(Collectors.toList());
-    }
-
-    @VisibleForTesting
-    static void validateNames(List<Statement> sortedStatements) {
-        IntStream.range(0, sortedStatements.size())
-                .forEachOrdered(i -> {
-                    String desiredName = String.valueOf(i + 1);
-                    Preconditions.checkArgument(desiredName.equals(sortedStatements.get(i).getName()),
-                            "Missing statement with name " + desiredName);
-                });
-    }
-
-    private static final String POLICY_TEMPLATE = "route-policy {$policy}\n"
-            + "{$statements|join(\n)}"
-            + "\n"
-            + "end-policy";
-
-    // Spaces in the template intentional to produce output comparable to xr
-    private static final String STATEMENT_TEMPLATE = "{% if ($conditions) %}"
-            + "{.if ($wasIf)}  else{.else}  {.endif}"
-            + "if {$conditions|join( and )} then\n"
-            + "    {$actions|join(\n    )}"
-            + "{% elseif ($actions) %}"
-            + "{.if ($wasIf)}  else\n    {$actions|join(\n    )}{.else}  {$actions|join(\n  )}{.endif}"
-            + "\n"
-            + "{% else %}"
-            + "{.if ($wasIf)}  endif\n{.endif}"
-            + "{% endif %}";
-
-    @VisibleForTesting
-    static String processStatements(List<Statement> statements, String rpName, CliFormatter format) {
-        boolean wasIf = false;
-        boolean wasElse = false;
-
-        List<String> statementStrings = new ArrayList<>();
-        for (Statement statement : statements) {
-            List<String> conditions = ConditionRenderer.renderConditions(statement.getConditions());
-            List<String> actions = ActionsRenderer.renderActions(statement.getActions(), statement.getConditions());
-
-            if (actions.isEmpty()) {
-                continue;
-            }
-
-            statementStrings.add(format.fT(STATEMENT_TEMPLATE,
-                    "wasIf", wasIf ? true : null,
-                    "conditions", !conditions.isEmpty() ? conditions : null,
-                    "actions", actions));
-
-            wasElse = wasIf && conditions.isEmpty();
-            wasIf = !conditions.isEmpty();
-        }
-        // Do one more empty statement, just to close previous if block if there was any
-        statementStrings.add(format.fT(STATEMENT_TEMPLATE,
-                "wasIf", wasIf || wasElse ? true : null,
-                "conditions", null,
-                "actions", null));
-
-        return format.fT(POLICY_TEMPLATE,
-                "policy", rpName,
-                "statements", statementStrings);
+            StatementsRenderer.renderStatements(statements, rpName));
     }
 
     @Override
@@ -144,9 +61,6 @@ public class StatementsWriter implements CliWriter<Statements> {
                                         @Nonnull WriteContext writeContext) throws WriteFailedException {
         // open and close the policy, this removes its body (statements)
         String rpName = id.firstKeyOf(PolicyDefinition.class).getName();
-        blockingDeleteAndRead(cli, id,
-                fT(POLICY_TEMPLATE,
-                        "policy", rpName,
-                        "statements", Collections.emptyList()));
+        blockingDeleteAndRead(cli, id, StatementsRenderer.renderStatements(null, rpName));
     }
 }
--- ios-xr/routing-policy/src/test/java/io/frinx/cli/unit/iosxr/routing/policy/handler/policy/StatementsWriterTest.java
+++ ios-xr/routing-policy/src/test/java/io/frinx/cli/unit/iosxr/routing/policy/handler/policy/StatementsWriterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018 Frinx and others.
+ * Copyright © 2019 Frinx and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,54 +16,71 @@
 
 package io.frinx.cli.unit.iosxr.routing.policy.handler.policy;
 
-import com.google.common.collect.ImmutableList;
-import java.util.List;
+import io.fd.honeycomb.translate.write.WriteContext;
+import io.fd.honeycomb.translate.write.WriteFailedException;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.io.Command;
+import io.frinx.openconfig.openconfig.policy.IIDs;
+import java.util.concurrent.CompletableFuture;
 import org.junit.Assert;
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.policy.statements.top.statements.Statement;
-import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.policy.statements.top.statements.StatementBuilder;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.policy.definitions.top.policy.definitions.PolicyDefinitionKey;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.routing.policy.rev170714.policy.statements.top.Statements;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.utils.IidUtils;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 public class StatementsWriterTest {
+    @Mock
+    private Cli cli;
+    @Mock
+    private WriteContext writeContext;
+    private StatementsWriter target;
+    private ArgumentCaptor<Command> response = ArgumentCaptor.forClass(Command.class);
 
-    @Rule
-    public final ExpectedException ee = ExpectedException.none();
+    private static final String WRITE_INPUT = "route-policy route_policy_3\n  apply abcd\nend-policy\n";
 
-    private static final Statement S_ILLEGAL = new StatementBuilder()
-            .setName("foo")
-            .build();
-    private static final Statement S1 = new StatementBuilder()
-            .setName("1")
-            .build();
-    private static final Statement S2 = new StatementBuilder()
-            .setName("2")
-            .build();
-    private static final Statement S3 = new StatementBuilder()
-            .setName("3")
-            .build();
+    private static final String DELETE_INPUT = "route-policy route_policy_3\nend-policy\n";
 
-    @Test
-    public void sortByNames() throws Exception {
-        List<Statement> sorted = StatementsWriter.sortByNames(ImmutableList.of(S2, S3, S1));
-        Assert.assertEquals(ImmutableList.of(S1, S2, S3), sorted);
+    private static final String POLICY_NAME = "route_policy_3";
+    private static final PolicyDefinitionKey POLICY_KEY = new PolicyDefinitionKey(POLICY_NAME);
+    private static final InstanceIdentifier<Statements> ID = IidUtils.createIid(IIDs.RO_PO_PO_STATEMENTS, POLICY_KEY);
+
+    private static final Statements DATA = StatementsReaderTest.getApplyStatements("abcd");
+    private static final Statements DATA_BEFORE = Mockito.mock(Statements.class);
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(cli.executeAndRead(Mockito.any())).then(invocation -> CompletableFuture.completedFuture(""));
+        target = new StatementsWriter(cli);
     }
 
     @Test
-    public void sortByNames_IllegalName() throws Exception {
-        ee.expect(IllegalArgumentException.class);
-        ee.expectMessage("Statement name must be a number but was foo");
-        StatementsWriter.sortByNames(ImmutableList.of(S2, S3, S_ILLEGAL));
+    public void testWriteCurrentAttributes() throws WriteFailedException {
+
+        target.writeCurrentAttributes(ID, DATA, writeContext);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(WRITE_INPUT, response.getValue().getContent());
     }
 
     @Test
-    public void validateNames() throws Exception {
-        // valid case
-        StatementsWriter.validateNames(ImmutableList.of(S1, S2, S3));
-        // non-valid case
-        ee.expect(IllegalArgumentException.class);
-        ee.expectMessage("Missing statement with name 2");
-        StatementsWriter.validateNames(ImmutableList.of(S1, S3));
+    public void testUpdateCurrentAttributes() throws WriteFailedException {
+
+        target.updateCurrentAttributes(ID, DATA_BEFORE, DATA, writeContext);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(WRITE_INPUT, response.getValue().getContent());
     }
 
+    @Test
+    public void testDeleteCurrentAttributes() throws WriteFailedException {
+
+        target.deleteCurrentAttributes(ID, DATA_BEFORE, writeContext);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(DELETE_INPUT, response.getValue().getContent());
+    }
 }
