60fbf7a0d2fee25f275852efbc1ef6a33c63bbbd
vklevlanyk <vklevlanyk@frinx.io>
2020-09-28 22:09:24 +0200

    VZ-235 - Parse port range in ciena devices
    Signed-off-by: vklevlanyk <vklevlanyk@frinx.io>
    Change-Id: Ib4ed75dee2427ec68b5d7b35bb384a00128b499a

--- saos/saos-6/saos-8-interface/src/test/java/io/frinx/cli/unit/saos8/ifc/handler/port/PortReaderTest.java
+++ saos/saos-6/saos-8-interface/src/test/java/io/frinx/cli/unit/saos8/ifc/handler/port/PortReaderTest.java
@@ -33,78 +33,113 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 public class PortReaderTest {
 
     public static final String SH_INTERFACE = "port set port 1/1 max-frame-size 9216\n"
-        + "port set port 1/2 max-frame-size 9216\n"
-        + "port disable port 2/1\n"
-        + "port set port 2/1 max-frame-size 9216\n"
-        + "port set port 2/3 max-frame-size 9216\n"
-        + "port disable port 2/4\n"
-        + "port disable port 2/5\n"
-        + "port set port 2/5 max-frame-size 9216\n"
-        + "port set port 2/6 max-frame-size 9216\n"
-        + "port disable port 2/7\n"
-        + "port disable port 2/8\n"
-        + "port set port 2/9 max-frame-size 9216 description TEMP_CS501-LM01WE\n"
-        + "port set port 2/10 max-frame-size 9216 description \"to-meetkamer CPE21-Gi0/0/0\"\n"
-        + "port set port 2/11 max-frame-size 9216 description TEMP_CS501-LM01W\n"
-        + "port set port 3/1 max-frame-size 9216\n"
-        + "port disable port 3/2\n"
-        + "port disable port 3/3\n"
-        + "port disable port 3/4\n"
-        + "port disable port 3/5\n"
-        + "port disable port 3/6\n"
-        + "port disable port 3/7\n"
-        + "port disable port 3/8\n"
-        + "port set port 3/9 description mnd-gt0002-spirent001-gi-1/0/4\n"
-        + "port set port 3/8 description Wytze\n"
-        + "port set port LP01 max-frame-size 9216\n"
-        + "port set port LM01E max-frame-size 9216\n"
-        + "port set port LM01W max-frame-size 9216\n"
-        + "port set port LS01E max-frame-size 9216\n"
-        + "port set port LS02E max-frame-size 9216\n"
-        + "port set port LP02 max-frame-size 9216\n"
-        + "port set port 2/3 speed ten-gig\n"
-        + "port set port 2/10 speed gigabit auto-neg on\n"
-        + "port set port 2/9 speed gigabit\n"
-        + "port set port 3/2 auto-neg on\n"
-        + "aggregation add agg LP02 port 2/2\n"
-        + "aggregation add agg LS02E port 2/3\n"
-        + "aggregation add agg LP01 port 2/5\n"
-        + "aggregation add agg LP01 port 2/6\n"
-        + "aggregation add agg LM01E port 2/9\n"
-        + "aggregation add agg LM01W port 2/11\n"
-        + "aggregation add agg LS01E port 3/1\n"
-        + "aggregation add agg JMEP port 3/7\n"
-        + "aggregation set port 3/6 agg-mode manual\n"
-        + "lldp set port 1/1-1/2 notification on\n"
-        + "lldp set port 2/1-2/20 notification on\n"
-        + "lldp set port 3/1-3/20 notification on\n"
-        + "snmp port-traps disable port 3/5 all\n"
-        + "snmp port-traps disable port 3/5 link-up-down-trap enhanced\n"
-        + "snmp port-traps disable port 3/5 link-up-down-trap standard\n"
-        + "snmp port-traps disable port 3/6 all\n"
-        + "snmp port-traps disable port 3/6 link-up-down-trap enhanced\n"
-        + "snmp port-traps disable port 3/6 link-up-down-trap standard\n"
-        + "snmp port-traps disable port JMEP all\n"
-        + "snmp port-traps disable port JMEP link-up-down-trap enhanced\n"
-        + "snmp port-traps disable port JMEP link-up-down-trap standard\n";
+            + "port set port 1/2 max-frame-size 9216\n"
+            + "port disable port 2/1\n"
+            + "port set port 2/1 max-frame-size 9216\n"
+            + "port set port 2/3 max-frame-size 9216\n"
+            + "port disable port 2/4\n"
+            + "port disable port 2/5\n"
+            + "port set port 2/5 max-frame-size 9216\n"
+            + "port set port 2/6 max-frame-size 9216\n"
+            + "port disable port 2/7\n"
+            + "port disable port 2/8\n"
+            + "port set port 2/9 max-frame-size 9216 description TEMP_CS501-LM01WE\n"
+            + "port set port 2/10 max-frame-size 9216 description \"to-meetkamer CPE21-Gi0/0/0\"\n"
+            + "port set port 2/11 max-frame-size 9216 description TEMP_CS501-LM01W\n"
+            + "port set port 3/1 max-frame-size 9216\n"
+            + "port disable port 3/2\n"
+            + "port disable port 3/3\n"
+            + "port disable port 3/4\n"
+            + "port disable port 3/5\n"
+            + "port disable port 3/6\n"
+            + "port disable port 3/7\n"
+            + "port disable port 3/8\n"
+            + "port set port 3/9 description mnd-gt0002-spirent001-gi-1/0/4\n"
+            + "port set port 3/8 description Wytze\n"
+            + "port set port LP01 max-frame-size 9216\n"
+            + "port set port LM01E max-frame-size 9216\n"
+            + "port set port LM01W max-frame-size 9216\n"
+            + "port set port LS01E max-frame-size 9216\n"
+            + "port set port LS02E max-frame-size 9216\n"
+            + "port set port LP02 max-frame-size 9216\n"
+            + "port set port 2/3 speed ten-gig\n"
+            + "port set port 2/10 speed gigabit auto-neg on\n"
+            + "port set port 2/9 speed gigabit\n"
+            + "port set port 3/2 auto-neg on\n"
+            + "aggregation add agg LP02 port 2/2\n"
+            + "aggregation add agg LS02E port 2/3\n"
+            + "aggregation add agg LP01 port 2/5\n"
+            + "aggregation add agg LP01 port 2/6\n"
+            + "aggregation add agg LM01E port 2/9\n"
+            + "aggregation add agg LM01W port 2/11\n"
+            + "aggregation add agg LS01E port 3/1\n"
+            + "aggregation add agg JMEP port 3/7\n"
+            + "aggregation set port 3/6 agg-mode manual\n"
+            + "lldp set port 1/1-1/2 notification on\n"
+            + "lldp set port 2/1-2/20 notification on\n"
+            + "lldp set port 3/1-3/20 notification on\n"
+            + "snmp port-traps disable port 3/5 all\n"
+            + "snmp port-traps disable port 3/5 link-up-down-trap enhanced\n"
+            + "snmp port-traps disable port 3/5 link-up-down-trap standard\n"
+            + "snmp port-traps disable port 3/6 all\n"
+            + "snmp port-traps disable port 3/6 link-up-down-trap enhanced\n"
+            + "snmp port-traps disable port 3/6 link-up-down-trap standard\n"
+            + "snmp port-traps disable port JMEP all\n"
+            + "snmp port-traps disable port JMEP link-up-down-trap enhanced\n"
+            + "snmp port-traps disable port JMEP link-up-down-trap standard\n";
 
     private static final String SH_AGG_IFACE =
-        "aggregation create agg LP01\n"
-        + "aggregation create agg LM01E\n"
-        + "aggregation create agg LM01W\n"
-        + "aggregation create agg LS01W\n"
-        + "aggregation create agg LS02W\n"
-        + "aggregation create agg LP02\n"
-        + "aggregation create agg JMEP\n"
-        + "aggregation create agg LSPIRENT01\n";
+            "aggregation create agg LP01\n"
+                    + "aggregation create agg LM01E\n"
+                    + "aggregation create agg LM01W\n"
+                    + "aggregation create agg LS01W\n"
+                    + "aggregation create agg LS02W\n"
+                    + "aggregation create agg LP02\n"
+                    + "aggregation create agg JMEP\n"
+                    + "aggregation create agg LSPIRENT01\n";
 
+    public static final String SH_RANGE_INTERFACE =
+            "port set port 1/1-1/5 max-frame-size 9216\n"
+                    + "port set port 1/18-2/3\n";
+
+    public static final String SH_DOUBLE_RANGE_INTERFACE =
+            "port set port 1/1-1/5,2/3-2/8\n"
+                    + "port set port 1/18-2/3,3/9 max-frame-size 9216\n";
+
+    public static final String SH_TRIPLE_RANGE_INTERFACE =
+            "port set port 1/1-1/5,2/3-2/8,2/8-3/1\n"
+                    + "port set port 1/18-2/3,2/3-2/8,3/9 max-frame-size 9216\n"
+                    + "port set port LP01\n";
     private static final List<InterfaceKey> IDS_EXPECTED = Lists.newArrayList("1/1", "1/2",
-        "2/1", "2/3", "2/4", "2/5", "2/6", "2/7", "2/8", "2/9", "2/10", "2/11",
-        "3/1", "3/2", "3/3", "3/4",  "3/5", "3/6", "3/7", "3/8", "3/9",
-        "LP01", "LM01E", "LM01W", "LS01E", "LS02E", "LP02", "2/2", "JMEP", "LS01W", "LS02W", "LSPIRENT01")
-        .stream()
-        .map(InterfaceKey::new)
-        .collect(Collectors.toList());
+            "2/1", "2/3", "2/4", "2/5", "2/6", "2/7", "2/8", "2/9", "2/10", "2/11",
+            "3/1", "3/2", "3/3", "3/4", "3/5", "3/6", "3/7", "3/8", "3/9", "2/2",
+            "LP01", "LM01E", "LM01W", "LS01E", "LS02E", "LP02", "JMEP", "LS01W", "LS02W", "LSPIRENT01")
+            .stream()
+            .map(InterfaceKey::new)
+            .distinct()
+            .collect(Collectors.toList());
+
+    private static final List<InterfaceKey> IDS_RANGE_EXPECTED = Lists.newArrayList("1/1", "1/18", "1/2",
+            "1/3", "1/4", "1/5", "1/19", "1/20", "2/1", "2/2", "2/3")
+            .stream()
+            .map(InterfaceKey::new)
+            .distinct()
+            .collect(Collectors.toList());
+
+    private static final List<InterfaceKey> IDS_DOUBLE_RANGE_EXPECTED = Lists.newArrayList("2/3", "3/9", "1/1",
+            "1/2", "1/3", "1/4", "1/5","2/4", "2/5", "2/6", "2/7", "2/8", "1/18", "1/19", "1/20", "2/1", "2/2")
+            .stream()
+            .map(InterfaceKey::new)
+            .distinct()
+            .collect(Collectors.toList());
+
+    private static final List<InterfaceKey> IDS_TRIPLE_RANGE_EXPECTED = Lists.newArrayList("2/8", "3/9",
+            "LP01", "1/1", "1/2", "1/3", "1/4", "1/5","2/3", "2/4", "2/5", "2/6", "2/7","2/9", "2/10", "2/11", "2/12",
+            "2/13", "2/14", "2/15", "2/16", "2/17", "2/18", "2/19", "2/20", "3/1", "1/18", "1/19", "1/20", "2/1", "2/2")
+            .stream()
+            .map(InterfaceKey::new)
+            .distinct()
+            .collect(Collectors.toList());
 
     @Test
     public void getAllIdsTest() throws ReadFailedException {
@@ -115,6 +150,42 @@ public class PortReaderTest {
                 .thenReturn(SH_INTERFACE, SH_AGG_IFACE);
 
         Assert.assertEquals(IDS_EXPECTED,
+                PortReader.getAllIds(null, cliReader, null, null));
+    }
+
+    @Test
+    public void getRangePortsTest() throws ReadFailedException {
+        CliReader cliReader = Mockito.mock(CliReader.class);
+
+        Mockito.when(cliReader.blockingRead(Mockito.anyString(), Mockito.any(Cli.class),
+                Mockito.any(InstanceIdentifier.class), Mockito.any(ReadContext.class)))
+                .thenReturn(SH_RANGE_INTERFACE);
+
+        Assert.assertEquals(IDS_RANGE_EXPECTED,
+                PortReader.getAllIds(null, cliReader, null, null));
+    }
+
+    @Test
+    public void getDoubleRangePortsTest() throws ReadFailedException {
+        CliReader cliReader = Mockito.mock(CliReader.class);
+
+        Mockito.when(cliReader.blockingRead(Mockito.anyString(), Mockito.any(Cli.class),
+                Mockito.any(InstanceIdentifier.class), Mockito.any(ReadContext.class)))
+                .thenReturn(SH_DOUBLE_RANGE_INTERFACE);
+
+        Assert.assertEquals(IDS_DOUBLE_RANGE_EXPECTED,
+                PortReader.getAllIds(null, cliReader, null, null));
+    }
+
+    @Test
+    public void getTripleRangePortsTest() throws ReadFailedException {
+        CliReader cliReader = Mockito.mock(CliReader.class);
+
+        Mockito.when(cliReader.blockingRead(Mockito.anyString(), Mockito.any(Cli.class),
+                Mockito.any(InstanceIdentifier.class), Mockito.any(ReadContext.class)))
+                .thenReturn(SH_TRIPLE_RANGE_INTERFACE);
+
+        Assert.assertEquals(IDS_TRIPLE_RANGE_EXPECTED,
                 PortReader.getAllIds(null, cliReader, null, null));
     }
 }
