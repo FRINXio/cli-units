ef117f179ca86cda3bee34d120277d2ae386d732
Vasyľ Klevľanyk <vklevlanyk@frinx.io>
2021-04-20 11:35:58 +0200

    VZ-461 - Added evc module to cli-units
    - Added evc module and test for it
    
    Signed-off-by: vklevlanyk <vklevlanyk@frinx.io>
    Change-Id: I40e58a49951d68c3a56b2d925a36a9b7f9138a49
    (cherry picked from commit fab944fdf022488b87f348ae728bee80ba134f8f)

---
+++ ios-xe/evc/src/main/java/io/frinx/cli/unit/iosxe/evc/handler/EvcReader.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2021 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.frinx.cli.unit.iosxe.evc.handler;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.fd.honeycomb.translate.read.ReadContext;
+import io.fd.honeycomb.translate.read.ReadFailedException;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.unit.utils.CliConfigListReader;
+import io.frinx.cli.unit.utils.ParsingUtils;
+import java.util.List;
+import java.util.regex.Pattern;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.evc.rev210507.evc.top.evcs.Evc;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.evc.rev210507.evc.top.evcs.EvcBuilder;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.evc.rev210507.evc.top.evcs.EvcKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class EvcReader implements CliConfigListReader<Evc, EvcKey, EvcBuilder> {
+
+    public static final String SH_EVCS = "show running-config | include ^ethernet evc";
+    private static final Pattern EVC_LINE = Pattern.compile("ethernet evc (?<name>\\S+).*");
+    private final Cli cli;
+
+    public EvcReader(Cli cli) {
+        this.cli = cli;
+    }
+
+    @Nonnull
+    @Override
+    public List<EvcKey> getAllIds(@Nonnull InstanceIdentifier<Evc> instanceIdentifier,
+                                  @Nonnull ReadContext readContext) throws ReadFailedException {
+        String output = blockingRead(SH_EVCS, cli, instanceIdentifier, readContext);
+        return getAllIds(output);
+    }
+
+    @VisibleForTesting
+    static List<EvcKey> getAllIds(String output) {
+        return ParsingUtils.parseFields(output, 0,
+            EVC_LINE::matcher,
+            matcher -> matcher.group("name"),
+            EvcKey::new);
+    }
+
+    @Override
+    public void readCurrentAttributes(@Nonnull InstanceIdentifier<Evc> instanceIdentifier,
+                                      @Nonnull EvcBuilder evcBuilder,
+                                      @Nonnull ReadContext readContext) throws ReadFailedException {
+        evcBuilder.setKey(instanceIdentifier.firstKeyOf(Evc.class));
+    }
+}
