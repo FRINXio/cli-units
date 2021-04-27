1b4fc9bae19b34666466a25b4ffea92121edff68
Simon Misencik <smisencik@frinx.io>
2021-04-27 13:20:27 +0200

    VZ-476 IOS: Add ios-xe version into routing policy unit
    Signed-off-by: Simon Misencik <smisencik@frinx.io>
    Change-Id: I783f8daf3d7e3ad881e14d9514069bbcb64eb7fd

--- ios/routing-policy/src/main/java/io/frinx/cli/unit/ios/routing/policy/RoutingPolicyUnit.java
+++ ios/routing-policy/src/main/java/io/frinx/cli/unit/ios/routing/policy/RoutingPolicyUnit.java
@@ -57,6 +57,7 @@ public class RoutingPolicyUnit extends AbstractUnit {
             {
                 add(IosDevices.IOS_12);
                 add(IosDevices.IOS_15);
+                add(IosDevices.IOS_XE_16);
             }
         };
     }
