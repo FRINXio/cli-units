e7e1d1995aaca14524d85cf8d3e965a0dcd8ba67
mmartiska <mmartiska@frinx.io>
2021-05-03 10:44:42 +0200

    VZ-474: Create TU for bfd-template in ios-xe
    Added refactor for bfd module in project.
    
    Signed-off-by: mmartiska <mmartiska@frinx.io>
    Change-Id: I35b86b324975e55ab42e5c24e2f02846688f2147

---
+++ ios-xe/bfd/src/main/java/io/frinx/cli/unit/iosxe/bfd/handler/BfdTemplateConfigWriter.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright © 2021 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.unit.iosxe.bfd.handler;
+
+import io.fd.honeycomb.translate.write.WriteContext;
+import io.fd.honeycomb.translate.write.WriteFailedException;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.unit.utils.CliWriter;
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.BfdTempAug;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.BfdTemplateConfig.Type;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.BfdTemplates;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.bfd.templates.BfdTemplate;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.bfd.templates.bfd.template.Config;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.bfd.templates.bfd.template.Interval;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class BfdTemplateConfigWriter implements CliWriter<BfdTempAug> {
+
+    private static final String TEMPLATE = "{% if ($bfdTemplateConfig) %}"
+            + "configure terminal\n"
+            + "{$bfdTemplateConfig}"
+            + "end"
+            + "{% endif %}";
+
+    private final Cli cli;
+
+    public BfdTemplateConfigWriter(Cli cli) {
+        this.cli = cli;
+    }
+
+    @Override
+    public void writeCurrentAttributes(@Nonnull InstanceIdentifier<BfdTempAug> instanceIdentifier,
+                                       @Nonnull BfdTempAug bfdTempAug,
+                                       @Nonnull WriteContext writeContext) throws WriteFailedException {
+        blockingWriteAndRead(cli, instanceIdentifier, bfdTempAug,
+                fT(TEMPLATE,
+                        "bfdTemplateConfig", getCommands(null, bfdTempAug)));
+    }
+
+    @Override
+    public void updateCurrentAttributes(@Nonnull InstanceIdentifier<BfdTempAug> instanceIdentifier,
+                                        @Nonnull BfdTempAug dataBefore,
+                                        @Nonnull BfdTempAug dataAfter,
+                                        @Nonnull WriteContext writeContext) throws WriteFailedException {
+        blockingWriteAndRead(cli, instanceIdentifier, dataAfter,
+                fT(TEMPLATE,
+                        "bfdTemplateConfig", getCommands(dataBefore, dataAfter)));
+    }
+
+    @Override
+    public void deleteCurrentAttributes(@Nonnull InstanceIdentifier<BfdTempAug> instanceIdentifier,
+                                        @Nonnull BfdTempAug bfdTempAug,
+                                        @Nonnull WriteContext writeContext) throws WriteFailedException {
+        blockingWriteAndRead(cli, instanceIdentifier, bfdTempAug,
+                fT(TEMPLATE,
+                        "bfdTemplateConfig", getCommands(bfdTempAug, null)));
+    }
+
+    private String getCommands(final BfdTempAug before, BfdTempAug after) {
+        final StringBuilder currentInstances = new StringBuilder();
+
+        if (before != null && after != null) {
+            final BfdTemplates bfdTemplatesBefore = before.getBfdTemplates();
+            final BfdTemplates bfdTemplatesAfter = after.getBfdTemplates();
+            if (bfdTemplatesBefore != null && bfdTemplatesAfter != null) {
+                final List<BfdTemplate> bfdTemplateBeforeList = bfdTemplatesBefore.getBfdTemplate();
+                final List<BfdTemplate> bfdTemplateAfterList = bfdTemplatesAfter.getBfdTemplate();
+                // Comparing list of changed bfd-templates with the list of original ones
+                if (bfdTemplateBeforeList != null && bfdTemplateAfterList != null) {
+                    for (BfdTemplate bfdtAfter : bfdTemplateAfterList) {
+                        for (BfdTemplate bfdtBefore : bfdTemplateBeforeList) {
+                            if (bfdtBefore.getConfig().getName().equals(bfdtAfter.getConfig().getName())) {
+                                if (!bfdtBefore.getConfig().getType().equals(bfdtAfter.getConfig().getType())
+                                    || bfdtBefore.getInterval() != bfdtAfter.getInterval()) {
+                                    currentInstances.append(getCommandsWhenDifferent(bfdtBefore, bfdtAfter));
+                                }
+                            }
+                        }
+                    }
+                    currentInstances.append("exit\n");
+                }
+            }
+        }
+        else if (before != null) {
+            final BfdTemplates bfdTemplates = before.getBfdTemplates();
+            if (bfdTemplates != null) {
+                final List<BfdTemplate> bfdTemplateList = bfdTemplates.getBfdTemplate();
+                if (bfdTemplateList != null) {
+                    for (final BfdTemplate bfdTemplate: bfdTemplateList) {
+                        currentInstances.append(getCreationCommands(bfdTemplate.getConfig(), true));
+                    }
+                }
+            }
+        }
+
+        else if (after != null) {
+            final BfdTemplates bfdTemplates = after.getBfdTemplates();
+            if (bfdTemplates != null) {
+                final List<BfdTemplate> bfdTemplateList = bfdTemplates.getBfdTemplate();
+                if (bfdTemplateList != null) {
+                    for (final BfdTemplate bfdTemplate : bfdTemplateList) {
+                        currentInstances.append(getCreationCommands(bfdTemplate.getConfig(), false));
+                        currentInstances.append(getIntervalCommands(bfdTemplate.getInterval()));
+                        currentInstances.append("exit\n");
+                    }
+                }
+            }
+        }
+        return currentInstances.toString();
+    }
+
+    private String getCreationCommands(final Config config, boolean delete) {
+        final StringBuilder creationCommands = new StringBuilder();
+        final String name = config != null ? config.getName() : null;
+        final Type type = config != null ? config.getType() : null;
+
+        if (delete) {
+            creationCommands.append("no ");
+        }
+
+        creationCommands.append("bfd-template ");
+        if (type != null) {
+            creationCommands.append(type.getName()).append(" ");
+        }
+        if (name != null) {
+            creationCommands.append(name);
+        }
+        creationCommands.append("\n");
+
+        return creationCommands.toString();
+    }
+
+    private String getIntervalCommands(final Interval interval) {
+        final StringBuilder configCommands = new StringBuilder();
+        if (interval != null) {
+            final String minTx = interval.getMinTx();
+            final String minRx = interval.getMinRx();
+            final String multiplier = interval.getMultiplier();
+            configCommands.append("interval");
+            if (minTx != null && minRx != null) {
+                configCommands.append(" min-tx ").append(minTx);
+                configCommands.append(" min-rx ").append(minRx);
+
+                if (multiplier != null) {
+                    configCommands.append(" multiplier ").append(multiplier);
+                }
+            }
+            configCommands.append("\n");
+        }
+        return configCommands.toString();
+    }
+
+    private String getCommandsWhenDifferent(BfdTemplate bfdTemplateBefore, BfdTemplate bfdTemplateAfter) {
+        final StringBuilder configCommands = new StringBuilder();
+
+        final Config bfdTemplateConfigBefore = bfdTemplateBefore.getConfig();
+        final Config bfdTemplateConfigAfter = bfdTemplateAfter.getConfig();
+
+        final Interval bfdTemplateIntervalAfter = bfdTemplateAfter.getInterval();
+        final Interval bfdTemplateIntervalBefore = bfdTemplateBefore.getInterval();
+
+        if (bfdTemplateConfigBefore != null && bfdTemplateConfigAfter != null) {
+            if (!bfdTemplateConfigBefore.getType().equals(bfdTemplateConfigAfter.getType())) {
+                configCommands.append(getCreationCommands(bfdTemplateConfigBefore, true));
+                configCommands.append(getCreationCommands(bfdTemplateConfigAfter, false));
+                configCommands.append(getIntervalCommands(bfdTemplateIntervalAfter));
+            }
+            else if (!bfdTemplateIntervalAfter.equals(bfdTemplateIntervalBefore)) {
+                configCommands.append(getCreationCommands(bfdTemplateConfigAfter, false));
+                if (bfdTemplateIntervalBefore != null) {
+                    configCommands.append("no ").append(getIntervalCommands(bfdTemplateIntervalBefore));
+                }
+                configCommands.append(getIntervalCommands(bfdTemplateIntervalAfter));
+            }
+        }
+        return configCommands.toString();
+    }
+
+
+}
---
+++ ios-xe/bfd/src/test/java/io/frinx/cli/unit/iosxe/bfd/handler/BfdTemplateConfigWriterTest.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright © 2021 Frinx and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.frinx.cli.unit.iosxe.bfd.handler;
+
+import io.fd.honeycomb.translate.write.WriteContext;
+import io.fd.honeycomb.translate.write.WriteFailedException;
+import io.frinx.cli.io.Cli;
+import io.frinx.cli.io.Command;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.BfdTempAug;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.BfdTempAugBuilder;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.BfdTemplateConfig.Type;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.BfdTemplates;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.BfdTemplatesBuilder;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.bfd.templates.BfdTemplate;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.bfd.templates.BfdTemplateBuilder;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.bfd.templates.BfdTemplateKey;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.bfd.templates.bfd.template.ConfigBuilder;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bfd.ext.rev180211.config.bfd.template.grouping.bfd.templates.bfd.template.IntervalBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
+
+public class BfdTemplateConfigWriterTest {
+
+    private static final List<BfdTemplate> WRITE_BFD_TEMPLATES =
+            Arrays.asList(
+                    new BfdTemplateBuilder()
+                        .setName("test")
+                        .setKey(new BfdTemplateKey("test"))
+                        .setConfig(new ConfigBuilder()
+                            .setName("test")
+                            .setType(Type.SingleHop)
+                            .build())
+                        .setInterval(new IntervalBuilder()
+                            .setMinTx("7")
+                            .setMinRx("5")
+                            .setMultiplier("3")
+                            .build())
+                        .build(),
+                    new BfdTemplateBuilder()
+                        .setName("aaa")
+                        .setConfig(new ConfigBuilder()
+                            .setName("aaa")
+                            .setType(Type.MultiHop)
+                            .build())
+                        .setInterval(new IntervalBuilder()
+                            .setMinTx("5")
+                            .setMinRx("6")
+                            .setMultiplier("3")
+                            .build())
+                        .build()
+            );
+
+    private static final List<BfdTemplate> UPDATE_BFD_TEMPLATE =
+            Collections.singletonList(
+                    new BfdTemplateBuilder()
+                            .setName("test")
+                            .setKey(new BfdTemplateKey("test"))
+                            .setConfig(new ConfigBuilder()
+                                    .setName("test")
+                                    .setType(Type.MultiHop)
+                                    .build())
+                            .setInterval(new IntervalBuilder()
+                                    .setMinTx("70")
+                                    .setMinRx("500")
+                                    .setMultiplier("3")
+                                    .build())
+                            .build()
+            );
+
+
+    private static final String WRITE_INPUT = "configure terminal\n"
+            + "bfd-template single-hop test\n"
+            + "interval min-tx 7 min-rx 5 multiplier 3\n"
+            + "exit\n"
+            + "bfd-template multi-hop aaa\n"
+            + "interval min-tx 5 min-rx 6 multiplier 3\n"
+            + "exit\n"
+            + "end\n";
+
+    private static final String UPDATE_INPUT = "configure terminal\n"
+            + "no bfd-template single-hop test\n"
+            + "bfd-template multi-hop test\n"
+            + "interval min-tx 70 min-rx 500 multiplier 3\n"
+            + "exit\n"
+            + "end\n";
+
+    private static final String DELETE_INPUT = "configure terminal\n"
+            + "no bfd-template multi-hop test\n"
+            + "end\n";
+
+    @Mock
+    private Cli cli;
+
+    @Mock
+    private WriteContext context;
+
+    private BfdTemplateConfigWriter writer;
+    private final ArgumentCaptor<Command> response = ArgumentCaptor.forClass(Command.class);
+    private final InstanceIdentifier iid = KeyedInstanceIdentifier.create(BfdTemplates.class)
+            .child(BfdTemplate.class, new BfdTemplateKey("test"));
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(cli.executeAndRead(Mockito.any())).then(invocation -> CompletableFuture.completedFuture(""));
+        writer = new BfdTemplateConfigWriter(cli);
+    }
+
+    @Test
+    public void write() throws WriteFailedException {
+        writer.writeCurrentAttributes(iid, getAug(WRITE_BFD_TEMPLATES), context);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(WRITE_INPUT, response.getValue().getContent());
+    }
+
+    @Test
+    public void update() throws WriteFailedException {
+        writer.updateCurrentAttributes(iid, getAug(WRITE_BFD_TEMPLATES), getAug(UPDATE_BFD_TEMPLATE),
+                context);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(UPDATE_INPUT, response.getValue().getContent());
+    }
+
+    @Test
+    public void delete() throws WriteFailedException {
+        writer.deleteCurrentAttributes(iid, getAug(UPDATE_BFD_TEMPLATE), context);
+        Mockito.verify(cli).executeAndRead(response.capture());
+        Assert.assertEquals(DELETE_INPUT, response.getValue().getContent());
+    }
+
+    private BfdTempAug getAug(final List<BfdTemplate> bfdTemplates) {
+        return new BfdTempAugBuilder()
+                .setBfdTemplates(new BfdTemplatesBuilder()
+                        .setBfdTemplate(bfdTemplates)
+                        .build())
+                .build();
+    }
+
+}
